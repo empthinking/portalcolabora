98802e13a98ca3aa5af763f7cd043bcd4f27458d
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-04-20 23:39:13 +0000

    js do cadastro

--- cadrastro.php
+++ cadrastro.php
@@ -23,15 +23,16 @@ require_once "header.php"
                       <label class="block text-gray-700 font-bold mb-2" for="senha">
                         Senha
                       </label>
-                      <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="senha" type="password" placeholder="Sua senha">
+                      <input class="password-input appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="senha" type="password" placeholder="Sua senha">
                       <p class="text-gray-600 text-xs italic">Sua senha deve ter pelo menos 8 caracteres.</p>
                     </div>
                     <div class="mb-4">
                       <label class="block text-gray-700 font-bold mb-2" for="confirma-senha">
                         Confirme sua senha
                       </label>
-                      <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirma-senha" type="password" placeholder="Confirme sua senha">
+                      <input class="password-input appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirma-senha" type="password" placeholder="Confirme sua senha">
                     </div>
+                    <button type="button" id="showPassword">Mostrar senha</button>
                     <div class="mb-4">
                       <label class="block text-gray-700 font-bold mb-2" for="telefone">
                         Telefone
@@ -44,5 +45,25 @@ require_once "header.php"
                       </button>
                     </div>
                   </form>
-                </div></div></div></section></main></body>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </body>
+      <script>
+      var showPasswordBtn = document.querySelector('.show-password-btn');
+var passwordInput = document.querySelector('.password-input');
+
+showPasswordBtn.addEventListener('click', function() {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    showPasswordBtn.textContent = 'Ocultar senha';
+  } else {
+    passwordInput.type = 'password';
+    showPasswordBtn.textContent = 'Mostrar senha';
+  }
+});
+
+</script>
 </html>                  
