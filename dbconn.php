f2d8b7d9fa01f4e6e4bfbe0835b817925e0924b8
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-21 01:59:00 -0300

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -6,8 +6,8 @@ define('SENHA', 'k8qXGC1|;M');
 define('DATABASE', 'u871226378_portalcolabora');
 $conn = mysqli_connect(SERVER, USUARIO, SENHA, DATABASE);
 
-if (!$conn) {
-    exit("Conexao falhou" . mysqli_connect_error());
-}
+#if (!$conn) {
+ #   exit("Conexao falhou" . mysqli_connect_error());
+#}
 
 ?>
