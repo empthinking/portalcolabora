a40c5a054cf2d3aacd3cda6ce15d2b2a4e86894a
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-21 20:33:05 -0300

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,17 +1,14 @@
 <?php
-$servername = "sql794.main-hosting.eu";
+$servername = "127.0.0.1";
 $user = "u871226378_colabora";
 $password = 'F7k|MYhYf>';
 $db_name = "u871226378_portalcolabora";
+$port = 3306;
 
-$conn = mysqli_connect($servername, $user, $password, $db_name);
-
-
-
-// Check connection
+$conn = mysqli_connect($servername, $user, $password, $db_name, $port);
+#Verificação de conexão
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+  echo "Falha na conexão com o banco de dados";
+  exit("Connection failed: " . mysqli_connect_error());
 }
-?>
-
 ?>
