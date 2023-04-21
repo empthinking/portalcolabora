ee4252b43b03ac430d58b7aed975afc7325e53e3
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-21 01:16:53 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,6 +1,105 @@
-<?php
-require_once "header.php"
-  ?>
+<?php 
+require_once "$pdo.php";
+$username = $password = $tel = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor coloque um nome de usuário.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "O nome de usuário pode conter apenas letras, números e sublinhados.";
+    } else{
+        // Prepare uma declaração selecionada
+        $sql = "SELECT id FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+            // Vincule as variáveis à instrução preparada como parâmetros
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            // Definir parâmetros
+            $param_username = trim($_POST["username"]);
+            
+            // Tente executar a declaração preparada
+            if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    $username_err = "Este nome de usuário já está em uso.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+
+            // Fechar declaração
+            unset($stmt);
+        }
+    }
+    
+    // Validar senha
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor insira uma senha.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "A senha deve ter pelo menos 6 caracteres.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validar e confirmar a senha
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Por favor, confirme a senha.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "A senha não confere.";
+        }
+    }
+    
+    // Verifique os erros de entrada antes de inserir no banco de dados
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare uma declaração de inserção
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+         
+        if($stmt = $pdo->prepare($sql)){
+            // Vincule as variáveis à instrução preparada como parâmetros
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            
+            // Definir parâmetros
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Tente executar a declaração preparada
+            if($stmt->execute()){
+                // Redirecionar para a página de login
+                header("location: login.php");
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+
+            // Fechar declaração
+            unset($stmt);
+        }
+    }
+    
+    // Fechar conexão
+    unset($pdo);
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Colabora</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,300,0,0" />    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" />
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+</head>
 <main>
         <section class="bg-gray-100 py-8">
             <div class="container mx-auto">
@@ -11,7 +110,7 @@ require_once "header.php"
                       <label class="block text-gray-700 font-bold mb-2" for="nome">
                         Nome completo
                       </label>
-                      <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nome" name="nome" type="text" placeholder="Seu nome completo">
+                      <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nome" name="username" type="text" value="<?php echo $username; ?>" placeholder="Seu nome completo">
                     </div>
                     <div class="mb-4">
                       <label class="block text-gray-700 font-bold mb-2" for="email">
@@ -23,14 +122,14 @@ require_once "header.php"
                       <label class="block text-gray-700 font-bold mb-2" for="senha">
                         Senha
                       </label>
-                      <input class="password-input appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="senha" name="senha" type="password" placeholder="Sua senha">
+                      <input class="password-input appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="senha" name="password" type="password" value="<?php echo $password; ?>" placeholder="Sua senha">
                       <p class="text-gray-600 text-xs italic">Sua senha deve ter pelo menos 8 caracteres.</p>
                     </div>
                     <div class="mb-4">
                       <label class="block text-gray-700 font-bold mb-2" for="confirma-senha">
                         Confirme sua senha
                       </label>
-                      <input class="password-input appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirma-senha" name="senha_conf" type="password" placeholder="Confirme sua senha">
+                      <input class="password-input appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirma-senha" name="confirm_password" type="password" value="<?php echo $confirm_password; ?>" placeholder="Confirme sua senha">
                     </div>
                     <button type="button" id="showPassword">Mostrar senha</button>
                     <div class="mb-4">
