0dd4e6c8ed4d44188af5e47bdcea59890c34e8cf
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-21 02:20:41 -0300

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "127.0.0.1:3306";
 $user = "u871226378_colabora"
-$password = 'F7k|MYhYf>'
-$db_name = "u871226378_portalcolabora"
+$password = 'F7k|MYhYf>';
+$db_name = "u871226378_portalcolabora";
 
 $conn = mysqli_connect($servername, $user, $password, $db_name);
 
