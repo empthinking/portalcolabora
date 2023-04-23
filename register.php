fb31730fd3e33254d280966a0a55b840ffaf0695
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-23 01:06:35 -0300

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
     
-		$name = $_POST["username"];
+		$username = $_POST["username"];
 		$email = $_POST["email"];
 		$password = $_POST["password"];
 		$number = $_POST["number"];
@@ -12,13 +12,17 @@
 		//regex para validar os dados
 		$username_reg = "/^[a-zA-Z ]+$/";
 		$number_reg = "/^[0-9]+$/";
-
+		//Verificacao de registro do email
+		$email_check->$mysqli->prepare("SELECT user_email FROM usuarios WHERE user_email = ?");
+		$email_check->bind_param('s', $email);
+		$email_check->execute();
+		
 		if(!preg_match($username_reg, $username) || empty($username)){ //Validacao do nome de usuario
 			$username_err = "Preencha o campo com letras, números ou sublinhado apenas";
 		} 
 		if(!filter_var($email, FILTER_VALIDATE_EMAIL) === false){ //Validacao de email
 			$email_err = "Insira um email válido";
-		} elseif($mysqli->num_rows("SELECT user_email FROM usuarios WHERE user_email = '$email'") > 0){
+		} elseif($email_check->num_rows > 0){
 			$email_err = "Email já cadastrado"; //Verificacao de registro do email
 		}
 		if(!preg_match($number_reg, $number) || strlen($number) != 11){ //Validacao do telefone
@@ -33,7 +37,7 @@
 
 		if(!isset($username_err) && !isset($email_err) && !isset($password_err) && !isset($number_err)){
 
-			$password_secure = password_hash($pass, PASSWORD_BCRYPT);
+			$password_secure = password_hash($password, PASSWORD_BCRYPT);
 			//Preparacao de declaracao SQL 
 			$stmt = $mysqli->prepare("INSERT INTO usuarios (user_nome, user_email, user_senha, user_tel) VALUES (?, ?, ?, ?)");
 			//Insercao das variaveis
