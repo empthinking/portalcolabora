d8a590241a7180f6306e9354395d9be1858dc6a5
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-23 02:32:10 -0300

    Update teste_de_cadastro.php

--- teste_de_cadastro.php
+++ teste_de_cadastro.php
@@ -16,18 +16,18 @@ require_once "register.php";
   <?php if(isset($username_err)){ echo "<p> $username_err </p>";} ?>
 
   <label for="email">Email:</label>
-  <input type="email" id="email" name="email"><br>
+  <input type="email" id="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>"><br>
   <?php if(isset($email_err)){ echo "<p> $email_err </p>";} ?>
 
   <label for="password">Password:</label>
-  <input type="password" id="password" name="password"><br>
+  <input type="password" id="password" name="password" value="<?php if(isset($_POST['password'])) echo htmlspecialchars($_POST['password']);?>"><br>
   <?php if(isset($password_err)){ echo "<p> $password_err </p>";} ?>
 
-  <label for="confirm-password">Confirm Password:</label>
+  <label for="confirm-password">Confirm Password:</label value="<?php if(isset($_POST['confirm_password'])) echo htmlspecialchars($_POST['confirm_password']);?>">
   <input type="password" id="confirm_password" name="confirm_password"><br>
 
   <label for="number">Number:</label>
-  <input type="tel" id="number" name="number"><br>
+  <input type="tel" id="number" name="number" value="<?php if(isset($_POST['number'])) echo htmlspecialchars($_POST['number']);?>"><br>
   <?php if(isset($number_err)){ echo "<p> $number_err </p>";} ?>
 
   <input type="submit" value="Submit">
