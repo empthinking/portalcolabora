dbc1fb2c5aa3f9cb02e65bdf223e13bf97e91e1e
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-23 01:19:56 -0300

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "127.0.0.1";
 $user = "u871226378_colabora";
 $password = 'F7k|MYhYf>';
