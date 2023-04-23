bb8c22a40bd5fd787e65039b1ea8e157ce0b7861
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-22 23:46:53 -0300

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php 
-	require_once "dbconn.php";
+	//require_once "dbconn.php";
 
 	$username = $password = $number = $confirm_password = $number = "";
 	$username_err = $email_err = $password_err = $number_err = "";
