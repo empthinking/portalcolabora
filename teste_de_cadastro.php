9d6404dae693bf7da31e2612f35a736a5694925b
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-23 01:50:32 -0300

    Update teste_de_cadastro.php

--- teste_de_cadastro.php
+++ teste_de_cadastro.php
@@ -12,22 +12,22 @@ require_once "register.php";
  <?php if(isset($msg)){ echo "<p>$msg</p>";} else { echo "Cadastro em pendência";} ?>
 <form action="teste_de_cadastro.php" method="post">
   <label for="username">Username:</label>
-  <input type="text" id="username" name="username"><br>
+  <input type="text" id="username" name="username" value="<?php if(isset()){ echo htmlspecialchars($username);}?>"><br>
   <?php if(isset($username_err)){ echo "<p> $username_err </p>";} ?>
 
   <label for="email">Email:</label>
-  <input type="email" id="email" name="email"><br>
+  <input type="email" id="email" name="email" value="<?php if(isset($email)){ echo htmlspecialchars($email);}?>"><br>
   <?php if(isset($email_err)){ echo "<p> $email_err </p>";} ?>
 
   <label for="password">Password:</label>
-  <input type="password" id="password" name="password"><br>
+  <input type="password" id="password" name="password" value="<?php if(isset($password)){ echo htmlspecialchars($password);}?>"><br>
   <?php if(isset($password_err)){ echo "<p> $password_err </p>";} ?>
 
   <label for="confirm-password">Confirm Password:</label>
-  <input type="password" id="confirm_password" name="confirm_password"><br>
+  <input type="password" id="confirm_password" name="confirm_password" value="<?php if(isset($confirm_password)){ echo htmlspecialchars($confirm_password);}?>"><br>
 
   <label for="number">Number:</label>
-  <input type="number" id="number" name="number"><br>
+  <input type="tel" id="number" name="number" value="<?php if(isset($number)){ echo htmlspecialchars($number);}?>"><br>
   <?php if(isset($number_err)){ echo "<p> $number_err </p>";} ?>
 
   <input type="submit" value="Submit">
