9354c3a3c69c4570cd70355c8414ce2a94bbe26b
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-23 23:35:02 -0300

    Update test2.php

--- test2.php
+++ test2.php
@@ -83,7 +83,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   </style>
 </head>
 <body>
-  <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  <form method="POST" action="./test2.php">
     <h2>User Registration</h2>
     <label for="user_nome">Name:</label>
     <input type="text" id="user_nome" name="user_nome" required>
