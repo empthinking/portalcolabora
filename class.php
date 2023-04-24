fee4d5c62c7716a695f9e97ad4154c56e5dac24a
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-23 21:10:50 -0300

    Adição da classe Usuario e mudança no script de registro

---
+++ class.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+class User {
+	private $username;
+	private $password;
+	private $email;
+	private $number;
+
+	function __construct(string $name, string $pwd, string $email, string $num){
+		if(empty($name) || empty($email) || empty($pwd) || empty($num))
+			throw new Exception('Todos os campos devem ser preenchidos');
+
+		if(!preg_match('/^[a-zA-Z ]+$/', $this->name))
+			throw new Exception('Nome deve conter apenas letras, números ou sublinhado apenas');
+
+		if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)
+			thow new Exception('Email inválido');
+
+		if(strlen($this->number) < 8 )
+			throw new Exception('Senha deve conter 8 ou mais caracteres');
+
+		if(!preg_match('/^[0-9]+$/', $this->number){
+			throw new Exception('Número Inválido');
+
+		$this->username = htmlspecialchars(trim($name));
+		$this->password = htmlspecialchars($pwd);
+		$this->email    = htmlspecialchars(trim($email));
+		$this->number   = htmlspecialchars(trim($num));
+
+	}
+	function get_name() : string {
+			return $this->username; 
+	}
+	function get_num() : string {
+			return $this->number;
+	}
+	function get_email() : string {
+			return $this->email;
+	}
+	function get_password() : string {
+			return $this->password;
+	}
+	function get_password_hash() : string {
+			return password_hash($this->password, PASSWORD_DEFAULT);
+	}
+
+}
+?>
--- register.php
+++ register.php
@@ -1,52 +1,34 @@
 <?php 
+	require_once "class.php";
 	require_once "dbconn.php";
+	$error_msg = "";
 
 	if($_SERVER["REQUEST_METHOD"] === "POST"){
-		$username = $_POST["username"];
-		$email = $_POST["email"];
-		$password = $_POST["password"];
-		$number = $_POST["number"];
-		$confirm_password = $_POST["confirm_password"];
-		//regex para validar os dados
-		$username_reg = "/^[a-zA-Z ]+$/";
-		$number_reg = "/^[0-9]+$/";
+		try{
+			$user = new User($_POST["username"], $_POST["password"], $_POST["email"], $_POST["number"]);
+			$confirm_password = htmlspecialchars($_POST["confirm_password"]);
+		} catch(Exception $error) {
+			$error_msg = $error->getMessage();
+		}
+
 		//Verificacao de registro do email
 		$email_check = $mysqli->prepare("SELECT user_email FROM usuarios WHERE user_email = ?");
-		$email_check->bind_param('s', $email);
+		$email_check->bind_param('s', $user->get_email());
 		$email_check->execute();
-		
-		if(!preg_match($username_reg, $username) || empty($username)){ //Validacao do nome de usuario
-			$username_err = "Preencha o campo com letras, números ou sublinhado apenas";
-		} 
-		if(filter_var($email, FILTER_VALIDATE_EMAIL) === false || empty($email)){ //Validacao de email
-			$email_err = "Insira um email válido";
-		} elseif($email_check->num_rows > 0){
-			$email_err = "Email já cadastrado"; //Verificacao de registro do email
-		}
-		if(!preg_match($number_reg, $number) || strlen($number) !== 11){ //Validacao do telefone
-			$number_err = "Insira um número válido";
-		} 
-		if(strlen($password) < 8){ //Validacao do tamanho da senha
-			$password_err = "Senha precisa conter no mínimo 8 caracteres";
-		} elseif($password !== $confirm_password){ //Confirmação da senha
-			$password_err = "Insira corretamente a confirmação";
-		}
 
+		if($user->get_password() !== $confirm_password) //Confirmação da senha
+			$error_msg = "Insira corretamente a confirmação";
 
-		if(!isset($username_err) && !isset($email_err) && !isset($password_err) && !isset($number_err)){
+		$stmt = $mysqli->prepare("INSERT INTO usuarios (user_nome, user_email, user_senha, user_tel) VALUES (?, ?, ?, ?)");
+		//Insercao das variaveis
+		$stmt->bind_param("ssss", $name, $email, $password_secure, $number);
+		//Envio dos dados
+		$stmt->execute();
+		//Encerramento da conexao
+		$stmt->close();
+		$msg = "Registro completado com sucesso";
 
-			$password_secure = password_hash($password, PASSWORD_BCRYPT);
-			//Preparacao de declaracao SQL 
-			$stmt = $mysqli->prepare("INSERT INTO usuarios (user_nome, user_email, user_senha, user_tel) VALUES (?, ?, ?, ?)");
-			//Insercao das variaveis
-			$stmt->bind_param("ssss", $name, $email, $password_secure, $number);
-			//Envio dos dados
-			$stmt->execute();
-			//Encerramento da conexao
-			$stmt->close();
-			$msg = "Registro completado com sucesso";
+	}
 
-		}
-}
 ?>
 
