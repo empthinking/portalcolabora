5dd30e0169142bcdcd20b454430c0469e89eaa00
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-24 03:46:48 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,54 +4,57 @@ session_start();
 
 require_once 'dbconn.php';
 
-$username = $password = '';
-$username_err = $password_err = $login_err = '';
+$email = $password = '';
+$email_err = $password_err = $login_err = '';
 
-$username_reg = '/^[a-zA-Z ]+$/';
-
-if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SESSION['loggedin'] != true){
-	$username = trim($_POST['username']);
+if($_SERVER['REQUEST_METHOD'] == 'POST' && !isset($_SESSION['login']){
+	$email = trim($_POST['email']);
 	$password = trim($_POST['password']);
 
-	if(empty(trim($username)) || !preg_match($username_reg, $username)){
-		$username_err = 'Insira o nome de usuário válido.';
+	if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$result = $mysqli->query("SELECT user_id FROM usuarios WHERE user_nome = '$email'");
 
-	} elseif($mysqli->num_rows("SELECT user_id FROM usuarios WHERE user_nome = '$username'") > 0) {
-		$username_err = 'Nome já registrado';
+	}
+	if($result->num_rows > 0) {
+		$email_err = 'Email já registrado';
 	}	
 	
 	if(strlen($password) < 8){
 		$password_err = 'Senha deve conter no mínimo 8 caracteres';
 	}
 
-	if(empty($username_err) && empty($password_err)){
-		$stmt = $mysqli->prepare('SELECT user_id, user_nome, user_senha FROM usuarios WHERE user_nome = ?');
-		$stmt->bind_param('s', $username);
+	if(empty($email_err) && empty($password_err) && $result->num_rows == 1){
+		$stmt = $mysqli->prepare('SELECT * FROM usuarios WHERE user_email = ?');
+		$stmt->bind_param('s', $email);
 		$stmt->execute();
 		$result = $stmt->get_result();
 		$row = $result->fetch_assoc();
 
-		if($result->num_rows === 1){
-			$id = $row['user_id'];
-			$username_stored = $row['user_nome'];
-			$password_stored = $row['user_senha'];
+		$id = $row['user_id'];
+		$username_stored = $row['user_nome'];
+		$password_stored = $row['user_senha'];
+		$email_stored = $row['user_email'];
+		$number_stored = $row['user_tel'];
+
 			
-			if(password_verify($password, $password_stored)){
-				session_start();
+		if(password_verify($password, $password_stored)){
 
-				$_SESSION['loggedin'] = true;
-				$_SESSION['id'] = $id;
-				$_SESSION['username'] = $username;
 
-				header('location: ' . htmlspecialchars($_SERVER['PHP_SELF']));
-                        } else {
-                            $login_err = 'Nome de usuário ou senha inválidos.';
-                        }
+			$_SESSION['login'] = true;
+			$_SESSION['id'] = $id;
+			$_SESSION['username'] = $username_stored;
+			$_SESSION['email'] = $email_stored;
+			$_SESSION['number'] = $number_stored;
 
+			header('location: index.php');
+		} else {
+		    $login_err = 'Nome de usuário ou senha inválidos.';
 		}
-		$stmt->close();
-		$result->free_result();
-	}	
+
+	}
+	$stmt->close();
+	$result->free_result();
+	
 }
 $mysqli->close();
 ?>
