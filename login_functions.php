c2729a1d10c7440974c36c91af6d71be6c0649c8
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-25 15:29:46 -0300

    Update login_functions.php

--- login_functions.php
+++ login_functions.php
@@ -1,45 +1,49 @@
 <?php
-function isUserLoggedIn() : bool{
-	return isset($_SESSION['login']) && $_SESSION['login'] === true);
-}
-function userLogin(string $email, string $pwd, mysqli $conn) : void {
-  if(!isset($conn))
-    throw new Exception('Ausencia do objeto mysqli como parametro');
-  
-	$stmt = $mysqli->prepare('SELECT * FROM usuarios WHERE user_email = ?');
-		$stmt->bind_param('s', $email);
-		if($stmt->execute()){
-      $result = $stmt->get_result();
-      $row = $result->fetch_assoc();
-			$result->free_result();
-    } else {
-      throw new Exception($conn->error);
+function isUserLoggedIn():
+    bool {
+        return isset($_SESSION['login']) && $_SESSION['login'] === true);
     }
+    function userLogin(string $email, string $pwd, mysqli $conn):
+        void {
+            if (!isset($email) || !isset($pwd)) throw new Exception('Campos de email e senha devem ser preenchidos');
+            if (!isset($conn)) throw new Exception('Ausencia do objeto mysqli como parametro');
 
-		$id = $row['user_id'];
-		if(verify_password($pwd, $row['user_senha'])){
-			$_SESSION['login'] = true;
-			$_SESSION['id'] = $row['user_id'];
-			$_SESSION['username'] = $row['user_nome'];
-			$_SESSION['email'] = $row['user_email'];
-			$_SESSION['number'] = $row['user_tel'];
-			session_regenerate_id(true);
+            $stmt = $mysqli->prepare('SELECT * FROM usuarios WHERE user_email = ?');
+            $stmt->bind_param('s', $email);
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                $result->free_result();
+            }
+            else {
+                throw new Exception($conn->error);
+            }
 
-      $timeout = 1800; // 30 minutow
-      session_set_cookie_params($timeout);
-			$stmt->close();
-      $$row = array();
+            $id = $row['user_id'];
+            if (verify_password($pwd, $row['user_senha'])) {
+                $_SESSION['login'] = true;
+                $_SESSION['id'] = $row['user_id'];
+                $_SESSION['username'] = $row['user_nome'];
+                $_SESSION['email'] = $row['user_email'];
+                $_SESSION['number'] = $row['user_tel'];
+                session_regenerate_id(true);
 
-} else {
-      throw new Exception('Nome de usuario ou senha não encontrado');
-    }
-  $conn->close();
-}
-function userLogout() : void {
-// unset all session variables
-$_SESSION = array();
-session_destroy();
-header("Location: index.php");
-exit;
-}
+                $timeout = 1800; // 30 minutos
+                session_set_cookie_params($timeout);
+                $stmt->close();
+                $row = array();
+
+            }
+            else {
+                throw new Exception('Nome de usuario ou senha não encontrado');
+            }
+            $conn->close();
+        }
+        function userLogout():
+            void {
+                $_SESSION = array();
+                session_destroy();
+                header("Location: index.php");
+                exit;
+            }
 ?>
