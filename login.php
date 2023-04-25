dd626ff8529fdb1551e05810ef1c0754e6e86559
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-25 15:25:31 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,14 @@
 <?php
 
 session_start();
-
+require_once 'login_functions.php';
 require_once 'dbconn.php';
 
-$email = $password = '';
-$email_err = $password_err = $login_err = '';
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && (!isset($_SESSION['login']) || $_SESSION['login'] !== false)){
+$email = $password = '';
+if($_SERVER['REQUEST_METHOD'] == 'POST' && (!isUserLoggedIn()){
 	$email = trim($_POST['email']);
-	$password = trim($_POST['password']);
+	$password = $_POST['password'];
 /*
 	if(filter_var($email, FILTER_VALIDATE_EMAIL)){
 		$email_err = 'Formato inválido';
@@ -19,38 +18,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && (!isset($_SESSION['login']) || $_SESS
 		$password_err = 'Senha deve conter no mínimo 8 caracteres';
 	}
 */
-	if(empty($email_err) && empty($password_err)){
-		$stmt = $mysqli->prepare('SELECT * FROM usuarios WHERE user_email = ?');
-		$stmt->bind_param('s', $email);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$row = $result->fetch_assoc();
-
-		$id = $row['user_id'];
-		$username_stored = $row['user_nome'];
-		$password_stored = $row['user_senha'];
-		$email_stored = $row['user_email'];
-		$number_stored = $row['user_tel'];
-
-			
-		if($password == $password_stored){
-
-
-			$_SESSION['login'] = true;
-			$_SESSION['id'] = $id;
-			$_SESSION['username'] = $username_stored;
-			$_SESSION['email'] = $email_stored;
-			$_SESSION['number'] = $number_stored;
-			 session_regenerate_id(true);
-
-               		$timeout = 1800; // 30 minutes
-               		session_set_cookie_params($timeout);
-			$stmt->close();
-			$result->free_result();
-
+			userLogin($email, $password, $mysqli);
 			header('location: index.php');
 		} else {
-		    $login_err = 'Nome de usuário ou senha inválidos.';
+		    throw new Exception('Nome de usuário ou senha inválidos.');
 		}
 
 	}
