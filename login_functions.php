97bbf678fb00a07d2cc7e0fa135d533288f08e3b
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-26 15:08:17 -0300

    Update login_functions.php

--- login_functions.php
+++ login_functions.php
@@ -1,51 +1,49 @@
 <?php
-function isUserLoggedIn():
-    bool {
-        return isset($_SESSION['login']) && $_SESSION['login'] === true);
+function isUserLoggedIn(): bool {
+    return isset($_SESSION['login']) && $_SESSION['login'] === true;
+}
+
+function userLogin(string $email, string $pwd, mysqli $conn): void {
+    if (!isset($email) || !isset($pwd)) throw new Exception('Campos de email e senha devem ser preenchidos');
+    if (!isset($conn)) throw new Exception('Ausencia do objeto mysqli como parametro');
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) throw new Exception ('Email em formato incorreto');
+    if (strlen($pwd) < 8) throw new Exception('Senha deve conter pelo menos 8 caracteres');
+
+    $stmt = $conn->prepare('SELECT * FROM usuarios WHERE user_email = ?');
+    $stmt->bind_param('s', $email);
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $result->free_result();
     }
-    function userLogin(string $email, string $pwd, mysqli $conn):
-        void {
-            if (!isset($email) || !isset($pwd)) throw new Exception('Campos de email e senha devem ser preenchidos');
-            if (!isset($conn)) throw new Exception('Ausencia do objeto mysqli como parametro');
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) throw new Exception ('Email em formato incorreto');
-            if (strlen($pwd) < 8) throw new Exception('Senha deve conter pelo menos 8 caracteres');
+    else {
+        throw new Exception($conn->error);
+    }
 
-            $stmt = $mysqli->prepare('SELECT * FROM usuarios WHERE user_email = ?');
-            $stmt->bind_param('s', $email);
-            if ($stmt->execute()) {
-                $result = $stmt->get_result();
-                $row = $result->fetch_assoc();
-                $result->free_result();
-            }
-            else {
-                throw new Exception($conn->error);
-            }
+    $id = $row['user_id'];
+    if (verify_password($pwd, $row['user_senha'])) {
+        $_SESSION['login'] = true;
+        $_SESSION['id'] = $row['user_id'];
+        $_SESSION['username'] = $row['user_nome'];
+        $_SESSION['email'] = $row['user_email'];
+        $_SESSION['number'] = $row['user_tel'];
+        session_regenerate_id(true);
 
-            $id = $row['user_id'];
-            if (verify_password($pwd, $row['user_senha'])) {
-                $_SESSION['login'] = true;
-                $_SESSION['id'] = $row['user_id'];
-                $_SESSION['username'] = $row['user_nome'];
-                $_SESSION['email'] = $row['user_email'];
-                $_SESSION['number'] = $row['user_tel'];
-                session_regenerate_id(true);
+        $timeout = 1800; // 30 minutos
+        session_set_cookie_params($timeout);
+        $stmt->close();
+        $conn->close();
+        $row = array();
+    }
+    else {
+        throw new Exception('Nome de usuario ou senha não encontrado');
+    }
+}
 
-                $timeout = 1800; // 30 minutos
-                session_set_cookie_params($timeout);
-                $stmt->close();
-                $row = array();
-
-            }
-            else {
-                throw new Exception('Nome de usuario ou senha não encontrado');
-            }
-            $conn->close();
-        }
-        function userLogout():
-            void {
-                $_SESSION = array();
-                session_destroy();
-                header("Location: index.php");
-                exit;
-            }
+function userLogout(): void {
+    $_SESSION = array();
+    session_destroy();
+    header("Location: index.php");
+    exit;
+}
 ?>
