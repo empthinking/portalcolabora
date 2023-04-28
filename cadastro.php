39551dc37ff07771c11c04fb87f85d88c44eceb2
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-28 11:44:35 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -85,7 +85,7 @@ endif;
                       <label class="block text-gray-700 font-bold mb-2" for="telefone">
                         Telefone
                       </label>
-                      <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telefone" name="tel" type="text" placeholder="Seu número de telefone">
+                      <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telefone" name="number" type="text" placeholder="Seu número de telefone">
                     </div>
                     <div class="flex items-center justify-between">
                       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submmit">
