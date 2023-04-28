0615f94cb05f2925822c4b1ded86e8f0bae92e97
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-28 12:22:50 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,14 @@
 <?php
 //adiciona as funcoes de login
-require_once 'functions/sign_in.php';
+//require_once 'functions/sign_in.php';
 
 //estabelece a conexao com o banco de dados
 //objeto $mysqli
 require_once 'database.php';
+
+function isUserLoggedIn(): bool {
+    return isset($_SESSION['login']) && $_SESSION['login'] === true;
+}
 
 $email = $password = '';
 
@@ -13,7 +17,56 @@ if(!isUserLoggedIn()){
     $email = $mysqli->real_escape_string($_POST['email']);
     $password = $mysqli->real_escape_string($_POST['password']);
     
-    sign_in($mysqli, $email, $password);
+    //Prepara uma declaracao SQL
+    $stmt = $mysqli->prepare('SELECT * FROM usuarios WHERE user_email = ?');
+
+    //Adiciona a string de email na variavel '?'
+    $stmt->bind_param('s', $email);
+
+    //Executa a declaracao e checa se foi executada com sucesso
+    if ($stmt->execute()):
+
+        //cria um objeto contendo os resultados da requisicao
+        $result = $stmt->get_result();
+        
+        //cria um array associativo contendo as informacoes obtidas
+        $row = $result->fetch_assoc();
+
+        //limpa os resultados do objeto
+        $result->free_result();
+    else: 
+        //Em caso de falha, envia o respectivo erro
+        throw new Exception($mysqli->error);
+    endif;
+
+    //ID do usuario
+    $id = $row['user_id'];
+
+    //Verifica se o usuario esta cadastrado e realiza o login
+    if (verify_password($pwd, $row['user_senha'])) {
+        $_SESSION['login'] = true;
+        $_SESSION['id'] = $row['user_id'];
+        $_SESSION['username'] = $row['user_nome'];
+        $_SESSION['email'] = $row['user_email'];
+        $_SESSION['number'] = $row['user_tel'];
+
+        //gera um novo ID para a sessao
+        session_regenerate_id();
+
+        //tempo limite ate a sessao expirar
+        session_set_cookie_params(3600);
+
+        //fecha a conexao com o banco de dados
+        $stmt->close();
+        $mysqli->close();
+
+        //limpa o array
+        $row = array();
+    }
+    else {
+        //em caso de falha a mensagem é jo
+        throw new Exception('Nome de usuario ou senha não encontrado');
+    }
     header('location: index.php');
 } else {
     throw new Exception('Nome de usuário ou senha inválidos.');
