5e287dd43214fa01df6a4e4e090ebc3d7bd8b1a6
empthinking <117464630+empthinking@users.noreply.github.com>
2023-04-28 15:23:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,6 @@ function isUserLoggedIn(): bool {
 //Inicia a sessao
 session_start();
 
-//Funcoes para a exibicao de mensagens em popup
-//require_once 'functions/message.php'
-
 //Checa se o formulaio de login foi enviado
 if ($_SERVER['REQUEST_METHOD'] === 'POST'):
 	require_once "login.php"; //executa login de usuario
@@ -18,19 +15,8 @@ endif;
 // Cabeçalho
 require_once 'header.php';
 
-//Checa se ha algum erro e exibe um popup com a respectiva mensagem
-if(isset($_SESSION['error'])):
-    echo errorMsg($_SESSION['error']);
-    unset($_SESSION['error']);
-
-//Checa se ha uma mensagem de confirmacao e exibe um popup
-elseif(isset($_SESSION['success'])):
-    echo errorMsg($_SESSION['success']);
-    unset($_SESSION['success']);
-endif;
-
 //Pagina de perfil
-require_once 'perfil.php';
+require_once 'home.php';
 
 //Rodape
 require_once 'footer.php';
