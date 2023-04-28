693ab0f0102f7f2de3b198f00d2894e846812293
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-04-28 17:22:56 -0300

    as

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <script src="scripts/script.js"></script>
-<footer class=bg-green-500>
-    <h1>rodapé</h1>
+<footer class="bg-green-500">
+<p class="mx-auto">Este é o rodapé</p>
 </footer>
 </html>
