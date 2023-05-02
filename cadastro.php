bda73a5eb6ef832922b61355b41e7d1041eaa2ed
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-02 14:44:39 -0300

    gg

--- cadastro.php
+++ cadastro.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") :
   $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
   //Validação local das senha no fomulário.
-  try {
+ 
     //Confirmação da senha
     if ($password !== $confirm_password) :
       throw new Exception('Insira corretamente a confirmação');
@@ -28,14 +28,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") :
       $stmt->close();
       $mysqli->close();
     endif;
-  } catch (Exception $error) {
-    $error_msg = $error->getMessage();
-  }
+
 endif;
 
 session_start();
 //Exibir mensagem de alerta de erro e sucesso
-if ($_SERVER["REQUEST_METHOD"] !== "POST") :
+if ($_SERVER["REQUEST_METHOD"] === "POST") :
   $_SESSION['success_msg'] = 'Registro completado com sucesso';
   echo '<script>alert("' . htmlspecialchars($_SESSION['success_msg']) . '")</script>';
   unset($_SERVER["REQUEST_METHOD"]);
