f406665df3592fb62f4729ee26e9f56361db9244
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-03 13:57:00 -0300

    asd

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 				</div>
 				<div class="navbar-menu md:flex items-center hidden">
 					
-					<div class="flex items-center py-2 -mx-4 md:mx-4">
+					<div class="flex items-center py-2 -mx-4 md:mx-4 max-w-full">
 						<div class="relative mr-10 md:mx-0">
 							<input class="bg-gray-200 rounded-full border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 pl-4 pr-10 py-2 w-40" type="text" placeholder="Pesquisar...">
 							<button class="absolute right-0 mt-2 mr-2">
