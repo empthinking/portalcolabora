cf9596b141c64a88d3a6ed3950905a4263c1345c
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-03 19:07:56 -0300

    asdasd

--- header_loggedin.php
+++ header_loggedin.php
@@ -45,7 +45,6 @@
                         <input class="bg-gray-200 rounded-full border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 pl-4 pr-10 py-2 w-80" type="text" placeholder="Pesquisar...">
                         <button class="absolute right-0 mt-2 mr-2">
 							<span class="material-symbols-outlined ">
-								search
 							</span>
 						</button>
                     </div>
