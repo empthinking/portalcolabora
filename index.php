adc02a0581b64fa129ccc38c4686c58634f99eed
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-03 17:54:14 -0300

    sdf

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 //função para verificar se o usuário está logado.
-function isUserLoggedIn(): bool {
+function isUserLoggedIn(): bool
+{
     return isset($_SESSION['login']) && $_SESSION['login'] === true;
 }
 
@@ -9,32 +10,33 @@ function isUserLoggedIn(): bool {
 session_start();
 
 //alerta de cadastro realizado
-if(isset($_SESSION['success_msg'])):
+if (isset($_SESSION['success_msg'])) :
     $msg = $_SESSION['success_msg'];
     echo '<script>alert("' . htmlspecialchars($msg) . '")</script>';
     unset($_SESSION['success_msg']);
 endif;
 
 //alerta de login
-if(isset($_SESSION['login_success']) && !isset($_SESSION['login_success_displayed'])):
+if (isset($_SESSION['login_success']) && !isset($_SESSION['login_success_displayed'])) :
     echo '<script>alert("' . htmlspecialchars('Login realizado com sucesso!') . '")</script>';
     $_SESSION['login_success_displayed'] = true;
     unset($_SESSION['login_success']);
-elseif(isset($_SESSION['login_error'])):
+elseif (isset($_SESSION['login_error'])) :
     echo '<script>alert("' . htmlspecialchars($_SESSION['login_error']) . '")</script>';
     unset($_SESSION['login_error']);
-    //$_SESSION['login_success_displayed'] = true;
+    echo "<script> document.getElementById('singIn').style.display='block'</script>";
+
 endif;
 //Checa se o formulaio de login foi enviado
-if ($_SERVER['REQUEST_METHOD'] === 'POST'):
-	require_once "login.php"; //executa login de usuario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+    require_once "login.php"; //executa login de usuario
 endif;
 
 // Cabeçalho
-if(isUserLoggedIn()):
-	require_once 'header_loggedin.php';
-else:
-	require_once 'header.php';
+if (isUserLoggedIn()) :
+    require_once 'header_loggedin.php';
+else :
+    require_once 'header.php';
 endif;
 
 //Pagina de perfil
@@ -42,4 +44,3 @@ require_once 'home.php';
 
 //Rodape
 require_once 'footer.php';
-?>
