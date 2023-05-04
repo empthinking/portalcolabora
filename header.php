f099206dcb517228f95754f065b14143b0df64f6
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-03 21:04:53 -0300

    asdasd

--- header.php
+++ header.php
@@ -1,17 +1,5 @@
 <?php
-if (isset($_POST['email']) || isset($_POST['password'])) :
-	if (strlen($_POST['email']) == 0) :
-		echo '<div class="alert alert-danger">' . 'Preencha seu e-mail' . '</div>';
-	endif;
-	if (strlen($_POST['password']) == 0) :
-		echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
-		<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-		<div>
-		  An example danger alert with an icon
-		</div>
-	  </div></div>';
-	endif;
-endif;
+
 ?>
 
 <!DOCTYPE html>
