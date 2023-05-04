0a2ca867f53c8483de5acf56f0780b712196df3e
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-04 10:30:33 -0300

    asdasd

--- header.php
+++ header.php
@@ -82,9 +82,9 @@ if (empty(trim($_POST["password"]))) {
 								<form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 									<div class="mb-4 form-group">
 										<label class="block font-bold mb-2" for="email">
-											Email: <span><?php echo $email_err ?></span>
+											Email: 
 
-											<input class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="exemplo@exemplo.com" id="email" name="email" class="form-control">
+											<input class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="exemplo@exemplo.com" id="email" name="email" class="form-control" value="<span><?php if(isset($email_err)):echo $email_err ?></span>">
 										</label>
 									</div>
 									<div class="mb-6 form-group">
