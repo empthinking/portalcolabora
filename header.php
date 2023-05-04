caa1c10da3ab655e5de51a3db57cd0a4d5633c75
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-04 14:18:07 -0300

    asasd

--- header.php
+++ header.php
@@ -59,7 +59,15 @@
 					<button class="bg-green-200 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full ml-2" onclick="document.getElementById('singIn').style.display='block'">
 						Entrar
 					</button>
-					<div id="singIn" class=" <?php echo isset($_SESSION['login_error']) ? 'block' : 'hidden';?> ">
+					<div id="singIn" class=" 
+					<?php
+					if (isset($_SESSION['login_error'])) :
+						echo 'block';
+					else : 
+						echo 'hidden';
+						unset($_SESSION['login_error']);
+					endif
+					?> ">
 						<div class="flex items-center justify-center min-h-screen menu-overlay absolute inset-0 	bg-gray-900" style="opacity: 0.9;">
 							<div class="bg-white rounded-lg w-full max-w-md mx-auto p-8">
 								<h1 class="text-3xl font-bold mb-8 text-center">
