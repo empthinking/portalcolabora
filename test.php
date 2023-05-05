18cd488f77c88a9748d9d49aa5052b098590a994
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-05 14:52:36 -0300

    att debug do código lucas

--- test.php
+++ test.php
@@ -56,7 +56,7 @@ $port = 3306;
 $mysqli = new mysqli($servername, $user, $password, $db_name, $port);
 #Verificação de conexão
 if ($mysqli->connect_errno) {
-  echo "Falha na conexão com o banco de dados ${mysqli->connect_error}";
+  echo "Falha na conexão com o banco de dados {$mysqli->connect_error}";
   exit();
 }
 if($_SERVER["REQUEST_METHOD"] == "POST"){
