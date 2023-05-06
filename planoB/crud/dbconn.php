7c3c83a990a2b6acdf5db3bf2a811d7f22666dfb
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-05 22:56:47 -0300

    Update dbconn.php

--- planoB/crud/dbconn.php
+++ planoB/crud/dbconn.php
@@ -3,8 +3,7 @@
 $serverName = "127.0.0.1";
 $dBUsername = "u871226378_colabora";
 $dBPassword = "F7k|MYhYf>";
-$dBName = "planoB";
-$port = 3306;
+$dBName = "u871226378_portalcolabora";
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName, 3306);
 
 if (!$conn)
