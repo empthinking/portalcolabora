db95d6cf64f8f6abab4fe7e67ad1d2694cadfceb
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-12 09:01:47 -0300

    \tt

---
+++ header2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colabora</title>
+    <!-- link de ícones -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- link css -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+    <div class="flex itens-center flex-wrap p-1 text-orange-300">
+        <div>
+            <a href="http://">
+                <img src="img/Ativo 1 black.png" alt="logo" class="h-10">
+            </a>
+        </div>
+        <button>
+            <i class="material-icons">menu</i>
+        </button>
+        <div class="search-wrapper">
+            <form >
+                <input type="text" placeholder="Pesquisar">
+                <button type="submit">
+                    <i class="material-icons">search</i>
+                </button>
+            </form>
+        </div>
+        <div >
+            <div class="menu">
+                <a href="">Início</a>
+                <a href="">Contato</a>
+                <a href="">FAQ</a>
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
