96eee381fd69392cb935510781128aeaf4136123
empthinking <117464630+empthinking@users.noreply.github.com>
2023-05-16 11:22:28 -0300

    Update login.php

--- OOP/classes/login.php
+++ OOP/classes/login.php
@@ -1,10 +1,9 @@
 <?php
-#require_once 'UserTable.php';
-#require_once 'User.php';
-require_once 'constants.php';
+require_once 'UserTable.php';
+require_once 'User.php';
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $user = new User();
-    if(!$user->setName($_POST[U_N]) || !$user->setPassword($_POST[U_P]) || !$user->setEmail($_POST[U_E]) || !$user->setNumber($_POST[U_NUM]) {
+    if(!$user->setName($_POST[U_N]) || !$user->setPassword($_POST[U_P]) || !$user->setEmail($_POST[U_E]) || !$user->setNumber($_POST[U_NUM])) {
         echo $user->error;
         exit();
     }
@@ -30,19 +29,19 @@ echo <<<EOL
     <form action="registration.php" method="POST">
       <div class="form-group">
         <label for="name">Name:</label>
-        <input type="text" class="form-control" id="name" name="{U_N}" pattern="^[a-zA-Z]+$" required placeholder="{$_POST[U_N]}>
+        <input type="text" class="form-control" id="name" name="{U_N}" pattern="^[a-zA-Z]+$" required placeholder="{isset($_POST[U_N]) ? $_POST[U_N] : ''}">
       </div>
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" name="{U_E}" required placeholder="{$_POST[U_E]}>
+        <input type="email" class="form-control" id="email" name="{U_E}" required placeholder="{isset($_POST[U_E]) ? $_POST[U_E] : ''}">
       </div>
       <div class="form-group">
         <label for="password">Password (at least 8 characters):</label>
-        <input type="password" class="form-control" id="password" name="{U_P}" pattern=".{8,}" required placeholder="{$_POST[U_P]}>
+        <input type="password" class="form-control" id="password" name="{U_P}" pattern=".{8,}" required placeholder="{isset($_POST[U_P]) ? $_POST[U_P] : ''}">
       </div>
       <div class="form-group">
         <label for="phone">Phone Number:</label>
-        <input type="tel" class="form-control" id="phone" name="{U_NUM}" required placeholder="{$_POST[U_NUM]}>
+        <input type="tel" class="form-control" id="phone" name="{U_NUM}" required placeholder="{isset($_POST[U_NUM]) ? $_POST[U_NUM] : ''}">
       </div>
       <button type="submit" class="btn btn-primary">Register</button>
     </form>
