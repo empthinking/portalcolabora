584cbb82b4c47d541680e71e50475c42f021f9ff
empthinking <117464630+empthinking@users.noreply.github.com>
2023-05-16 10:47:40 -0300

    Update Table.php

--- OOP/classes/Table.php
+++ OOP/classes/Table.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'db.php';
 
-abstract class Table {
+class Table {
 
     protected $table_name;
     protected $db;
@@ -12,101 +12,31 @@ abstract class Table {
         $this->table_name = $table_name;
     }    
 
-    // Executa uma query
-    protected function sqlQuery(string $sql) : bool {
-        if(!$this->db->query($sql)){
-            $this->error = $this->db->error;
-            return FALSE;    
-        }
-        return TRUE;
-    }
-    
-
     // Realiza uma query preparada
-    protected function secureSqlQuery(string $sql_prep, mixed ...$variables) : bool{
+    protected function secureSqlQuery(string $sql_prep, array $bindings, bool $return = FALSE, bool $mult_row = FALSE) : bool | array {
         if(!$stmt = $this->db->prepare($sql_prep)){
             $this->error = $stmt->error;
             $stmt->close();
             return FALSE;
         }
 
-        if(!$stmt->execute($variables)){
+        if(!$stmt->execute($bindings)){
             $this->error = $stmt->error;
             $stmt->close();
             return FALSE;
         }
+
+        if($return){
+            $result = $stmt->get_result();
+            $stmt->close();
+            $row = $mult_result ? $result->fetch_all(MYSQLI_BOTH) : $result->fetch_assoc();
+            $result->close();
+            return $row;
+	}
 
         return TRUE;
     }
 
-    //Retorna um array multidimensional contendo as linhas requistadas
-    protected function getRows(string $sql) : array | bool {
-        $sql = $this->db->real_escape_string($sql);
-
-        if(!$result = $this->db->query($sql)){
-            $this->error = $this->db->error;
-            return FALSE;
-        }
-        while($row = $result->fetch_assoc())
-            $table[] = $row;
-        $result->close();
-        return $table;
-    }
-
-    //Retorna uma única linha requisitada
-    protected function getOneRow(string $sql) : array | bool {
-        $sql = $this->db->real_escape_string($sql);
-
-        if(!$result = $this->db->query($sql)) {
-            $this->error = $this->db->error;
-            return FALSE;
-        }
-
-        $row = $result->fetch_assoc();
-        $result->close();
-        return $row;
-    }
-    
-    protected function secureGetOneRow(string $sql_prep, mixed ...$variables) : array | bool {
-        if(!$stmt = $this->db->prepare($sql_prep)){
-            $this->error = $stmt->error;
-            $stmt->close();
-            return FALSE;
-        }
-
-        if(!$stmt->execute($variables)){
-            $this->error = $stmt->error;
-            $stmt->close();
-            return FALSE;
-        }
-
-        $result = $stmt->get_result();
-        $stmt->close();
-        $row = $result->fetch_assoc();
-        $result->close();
-        return $row;
-    }
-
-    protected function secureGetRows(string $sql_prep, mixed ...$variables) : array | bool {
-        if(!$stmt = $this->msqli->prepare($sql_prep)) {
-            $this->error = $stmt->error;
-            $stmt->close();
-            return FALSE;
-        }
-
-        if(!$stmt->execute($variables)) {
-            $this->error = $stmt->error;
-            $stmt->close();
-            return FALSE;
-        }
-
-        $result = $stmt->get_result();
-        $stmt->close();
-        while($row = $result->fetch_assoc())
-            $table[] = $row;
-        $result->close();
-        return $table;
-    }
 
     function __destruct(){
         $this->db->close();
