50f2cbc4f42805ea07745dc8cad7fe480d0b11c6
empthinking <117464630+empthinking@users.noreply.github.com>
2023-05-16 13:20:26 -0300

    Update db.php

--- OOP/classes/db.php
+++ OOP/classes/db.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'constants.php';
 
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$mysqli = new mysqli(HOST, NAME, PASSWORD, DATABASE);
 
 if($mysqli->connect_error)
     exit('Falha na conexão');
