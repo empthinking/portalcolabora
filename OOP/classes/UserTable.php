41c10bc2d6fd4660f4b7f618f872a9d7ef9b2fdf
empthinking <117464630+empthinking@users.noreply.github.com>
2023-05-16 11:57:41 -0300

    Update UserTable.php

--- OOP/classes/UserTable.php
+++ OOP/classes/UserTable.php
@@ -1,33 +1,33 @@
 <?php
 require_once 'Table.php';
-require_once 'User.php';
 
 class UserTable extends Table {
 
-    private table_name;
+    private $table_name;
 
     //Colunas
-    private id;
-    private username;
-    private email;
-    private password;
-    private number;
+    private $user_id;
+    private $username;
+    private $email;
+    private $password;
+    private $number;
 
     function __construct(
         mysqli $db,
         string $table_name = U_TABLE,
-        int    $id         = U_ID,
+        string $id         = U_ID,
         string $username   = U_N,
         string $email      = U_E,
         string $password   = U_P,
         string $number     = U_NUM
     ) {
-        parent::__construct($db, $table_name);
-        $this->id       = $id;
-        $this->username = $username;
-        $this->email    = $email;
-        $this->password = $password;
-        $this->number   = $number;
+        parent::__construct($db);
+	$this->table_name = $table_name;
+        $this->user_id    = $id;
+        $this->username   = $username;
+        $this->email      = $email;
+        $this->password   = $password;
+        $this->number     = $number;
     }
 
     private function emailVerify(string $email) : bool{
@@ -43,12 +43,12 @@ class UserTable extends Table {
 
     public function getUserByEmail(string $email) : bool | User {
         $sql_prep = "SELECT * FROM $this->table_name where $this->email = ? LIMIT 1";
-        if (!$row = $this->secureGetOneRow($sql_prep, $email))
+        if (!$row = $this->secureSqlQuery($sql_prep, array($email), TRUE))
             return FALSE;
 
         $user = new User();
 
-        $user->setId($row[(string)$this->id]);
+        $user->setId($row[$this->user_id]);
         $user->setUsername($row[$this->username]);
         $user->setEmail($row[$this->email]);
         $user->setPassword($row[$this->password]);
@@ -68,23 +68,23 @@ class UserTable extends Table {
         } 
 
         $sql_prep = "SELECT * FROM $this->table_name WHERE $this->email = ?";
-        $row      = $this->secureGetOneRow($sql_prep, $email);
+        $row      = $this->secureSqlQuery($sql_prep, $email, TRUE);
 
-        if(!password_verify($row[$this->password], $password) {
-            $error = 'Email ou Senha inálido';
+        if(!password_verify($row[$this->password], $password)) {
+            $this->error = 'Email ou Senha inálido';
             return FALSE;
         }
 
-        $_SESSION[(string)$this->id] = $row[(string)$this->id];
-        $_SESSION[$this->username]   = $row[$this->username];
-        $_SESSION[$this->email]      = $row[$this->email];
-        $_SESSION[$this->number]     = $row[$this->number];
+        $_SESSION[$this->user_id]  = $row[$this->user_id];
+        $_SESSION[$this->username] = $row[$this->username];
+        $_SESSION[$this->email]    = $row[$this->email];
+        $_SESSION[$this->number]   = $row[$this->number];
     }
 
     public function register(User $user) : bool {
         $username = $user->getUserName();
         $email    = $user->getEmail();
-        $password = $user->getPassword();
+        $password = password_hash($user->getPassword(), PASSWORD_DEFAULT);
         $number   = $user->getNumber();
 
         if($this->emailVerify($email)) {
@@ -93,7 +93,7 @@ class UserTable extends Table {
         }
 
         $sql = "INSERT INTO $this->table_name($this->username, $this->email, $this->password, $this->number) VALUES (?, ?, ?, ?)"; 
-        return $this->secureSqlQuery($sql, $username, $email, $password, $number);
+        return $this->secureSqlQuery($sql, array($username, $email, $password, $number));
     }
 
     public function update(User $user) : bool {
@@ -107,13 +107,13 @@ class UserTable extends Table {
             $this->number   => $user->getNumber()
         );
 
-        foreach($dataToUpdate as $table => $data) {
-            if(!$data || empty($data))
+        foreach($dataToUpdate as $column => $value) {
+            if(!$value || empty($value))
                 continue;
 
-            $sql = "UPDATE FROM $this->table_name SET $table = ? WHERE $this->id = ?";
+            $sql_prep = "UPDATE $this->table_name SET $column = ? WHERE $this->user_id = ?";
 
-            if(!$this->secureSqlQuery($sql, $data, $id))
+            if(!$this->secureSqlQuery($sql_prep, array($value, $user_id)))
                 return FALSE;
 
         }
@@ -122,7 +122,7 @@ class UserTable extends Table {
     }
 
     public function deleteUser(int $id) : bool {
-        $sql_prep = "DELETE FROM $this->table_name WHERE $this->id = ?";
-        return = $this->secureSqlQuery($sql_prep, $id);
+        $sql_prep = "DELETE FROM $this->table_name WHERE $this->user_id = ?";
+        return $this->secureSqlQuery($sql_prep, array($id));
     }
 }
