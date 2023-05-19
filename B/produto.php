2589b052c6060b13284b7ee67adf0734745a6d5a
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-19 13:17:32 -0300

    Update produto.php

--- B/produto.php
+++ B/produto.php
@@ -1,36 +1,58 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <title>Detalhes do Produto</title>
-  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css">
 </head>
-<body>
-  <div class="container">
-    <?php
-    // Verifica se o ID do produto foi fornecido na URL
-    if (isset($_GET['id'])) {
-      // Obtém o ID do produto da URL
-      $produto_id = $_GET['id'];
 
-      // Aqui você pode obter as informações do produto a partir do banco de dados ou de qualquer outra fonte de dados
+<body class="bg-gray-100">
+  <div class="container mx-auto py-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <!-- Imagem do produto -->
+      <div class="max-w-xs mx-auto">
+        <img src="caminho_da_imagem_do_produto" alt="Imagem do Produto" class="w-full">
+      </div>
 
-      // Exemplo de informações do produto
-      $nome = "Nome do Produto";
-      $descricao = "Descrição do produto.";
-      $preco = 99.99;
-      $imagem = "caminho/para/imagem.jpg";
+      <!-- Detalhes do produto -->
+      <div>
+        <h2 class="text-2xl font-bold">Nome do Produto</h2>
+        <p class="text-gray-500">Por: Nome do Criador</p>
+        <button onclick="mostrarContato()" class="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600">Entrar em Contato</button>
 
-      // Exibindo as informações do produto
-      echo "<h1>$nome</h1>";
-      echo "<p>$descricao</p>";
-      echo "<p>Preço: R$ $preco</p>";
-      echo "<img src='$imagem' alt='Imagem do Produto'>";
-    } else {
-      // Se o ID do produto não foi fornecido na URL, exiba uma mensagem de erro
-      echo "<p>Nenhum ID de produto fornecido.</p>";
-    }
-    ?>
+        <!-- Informações de contato (inicialmente ocultas) -->
+        <div id="contato" class="hidden mt-4">
+          <p>Número de telefone: XXXXXXXXX</p>
+        </div>
+
+        <h3 class="text-lg font-bold mt-6">Descrição</h3>
+        <p class="mt-2">Descrição do produto...</p>
+      </div>
+    </div>
+
+    <!-- Produtos recomendados -->
+    <h3 class="text-2xl font-bold mt-12 mb-4">Produtos Recomendados</h3>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <!-- Card de produto recomendado -->
+      <div class="bg-white rounded shadow p-4">
+        <img src="caminho_da_imagem_do_produto_recomendado" alt="Imagem do Produto Recomendado" class="w-full mb-4">
+        <h4 class="text-lg font-bold">Nome do Produto Recomendado</h4>
+        <p class="text-gray-500">Por: Nome do Criador</p>
+        <button onclick="mostrarContato()" class="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600">Entrar em Contato</button>
+      </div>
+
+      <!-- Repita os cards de produto recomendado aqui -->
+    </div>
   </div>
+
+  <script>
+    function mostrarContato() {
+      var contato = document.getElementById("contato");
+      contato.classList.toggle("hidden");
+    }
+  </script>
 </body>
+
 </html>
