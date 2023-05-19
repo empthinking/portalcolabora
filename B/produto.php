857f9227d97413caf3e821fb955d15d20d03b674
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-19 13:09:54 -0300

    Update produto.php

--- B/produto.php
+++ B/produto.php
@@ -3,62 +3,34 @@
 <head>
   <meta charset="UTF-8">
   <title>Detalhes do Produto</title>
+  <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-<?php
-// Função para obter produtos recomendados aleatoriamente
-function getProdutosRecomendados($conn, $id, $limit = 3) {
-    $query = "SELECT * FROM produtos WHERE id != $id ORDER BY RAND() LIMIT $limit";
-    $result = mysqli_query($conn, $query);
-    $produtos = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        $produtos[] = $row;
-    }
-    return $produtos;
-}
+  <div class="container">
+    <?php
+    // Verifica se o ID do produto foi fornecido na URL
+    if (isset($_GET['id'])) {
+      // Obtém o ID do produto da URL
+      $produto_id = $_GET['id'];
 
-// Verifica se foi passado um ID de produto válido na URL
-if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-    // Conecta ao banco de dados e faz a consulta para obter os detalhes do produto
-    $conn = mysqli_connect('localhost', 'usuario', 'senha', 'meu_banco_de_dados');
-    $id = $_GET['id'];
-    $query = "SELECT * FROM produtos WHERE id = $id";
-    $result = mysqli_query($conn, $query);
+      // Aqui você pode obter as informações do produto a partir do banco de dados ou de qualquer outra fonte de dados
 
-    // Verifica se a consulta retornou algum resultado
-    if (mysqli_num_rows($result) > 0) {
-        $produto = mysqli_fetch_assoc($result);
-        ?>
-        <h1><?php echo $produto['nome']; ?></h1>
-        <p><?php echo $produto['descricao']; ?></p>
-        <p>Preço: R$ <?php echo $produto['preco']; ?></p>
-        <p>Quantidade em estoque: <?php echo $produto['estoque']; ?></p>
-        <!-- Adicione aqui outros detalhes do produto que deseja exibir -->
-        <?php
+      // Exemplo de informações do produto
+      $nome = "Nome do Produto";
+      $descricao = "Descrição do produto.";
+      $preco = 99.99;
+      $imagem = "caminho/para/imagem.jpg";
 
-        // Obter produtos recomendados aleatoriamente
-        $produtosRecomendados = getProdutosRecomendados($conn, $id);
-
-        if (!empty($produtosRecomendados)) {
-            ?>
-            <h2>Produtos Recomendados</h2>
-            <ul>
-                <?php foreach ($produtosRecomendados as $recomendado) { ?>
-                    <li><?php echo $recomendado['nome']; ?></li>
-                    <!-- Adicione aqui os detalhes dos produtos recomendados que deseja exibir -->
-                <?php } ?>
-            </ul>
-            <?php
-        }
+      // Exibindo as informações do produto
+      echo "<h1>$nome</h1>";
+      echo "<p>$descricao</p>";
+      echo "<p>Preço: R$ $preco</p>";
+      echo "<img src='$imagem' alt='Imagem do Produto'>";
     } else {
-        echo "Produto não encontrado.";
+      // Se o ID do produto não foi fornecido na URL, exiba uma mensagem de erro
+      echo "<p>Nenhum ID de produto fornecido.</p>";
     }
-
-    // Fecha a conexão com o banco de dados
-    mysqli_close($conn);
-} else {
-    echo "ID de produto inválido.";
-}
-?>
+    ?>
+  </div>
 </body>
 </html>
