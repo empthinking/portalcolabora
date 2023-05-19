d6ca792f346da708523d43fabace744e0ea9e240
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-18 23:43:21 -0300

    Update index.php

--- B/index.php
+++ B/index.php
@@ -1,39 +1,7 @@
 <?php
-
 require_once "dbconn.php";
 require_once "funcoes.php";
-function isUserLoggedIn(): bool {
-    return isset($_SESSION['login']) && $_SESSION['login'] === true;
-}
-
-//Inicia a sessao
-session_start();
-
-if(isset($_SESSION['success_msg'])):
-    $msg = $_SESSION['success_msg'];
-    echo '<script>alert("' . htmlspecialchars($msg) . '")</script>';
-endif;
-
-//Checa se o formulaio de login foi enviado
-if ($_SERVER['REQUEST_METHOD'] === 'POST'):
-	require_once "login.php"; //executa login de usuario
-endif;
-
-// Cabeçalho
-if(isUserLoggedIn()):
-	require_once 'header_loggedin.php';
-else:
-	require_once 'header.php';
-endif;
-
-//Pagina de perfil
-// require_once 'home.php';
-
-//Rodape
-// require_once 'footer.php';
-
-
-
+require_once 'header.php';
 // Configurações da paginação
 $produtosPorPagina = 10;
 $paginaAtual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
