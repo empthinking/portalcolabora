148dfb7d92e21e33a9b400af955d9bf5c92f30f5
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-18 21:07:50 -0300

    Update database.php
    atualizar o banco

--- B/database.php
+++ B/database.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
-$user = "root";
-$password = '';
-$db_name = "crud";
+$servername = "127.0.0.1";
+$user = "u871226378_colabora";
+$password = 'F7k|MYhYf>';
+$db_name = "u871226378_portalcolabora";
 $port = 3306;
 
 $mysqli = new mysqli($servername, $user, $password, $db_name, $port);
