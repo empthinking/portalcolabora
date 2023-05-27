9a75e9f81f0996e1b927eb1883069470874fb977
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-27 11:32:34 -0300

    .

--- produto.php
+++ produto.php
@@ -1,14 +1,19 @@
 <?php
 require_once "dbconn.php";
 
-// Faz a requisição ao banco de dados para obter as informações do produto
-$sql = "SELECT imagem, nome, descricao, preco FROM produtos";
-$result = $conn->query($sql);
+// Verifica se o parâmetro "id" está presente na URL
+if (isset($_GET['id'])) {
+  // Obtém o ID do produto da URL
+  $produto_id = $_GET['id'];
 
-// Verifica se existem registros retornados
-if ($result->num_rows > 0) {
-  // Loop através dos registros e exibe as informações
-  while ($row = $result->fetch_assoc()) {
+  // Faz a requisição ao banco de dados para obter as informações do produto com o ID correspondente
+  $sql = "SELECT imagem, nome, descricao, preco FROM produtos WHERE id = $produto_id";
+  $result = $conn->query($sql);
+
+  // Verifica se existe um registro correspondente ao ID
+  if ($result->num_rows > 0) {
+    // Obtém os detalhes do produto
+    $row = $result->fetch_assoc();
     $imagem = $row["imagem"];
     $nome = $row["nome"];
     $descricao = $row["descricao"];
@@ -21,11 +26,13 @@ if ($result->num_rows > 0) {
     echo "<p>$descricao</p>";
     echo "<p>Preço: R$ $preco</p>";
     echo "</div>";
+  } else {
+    echo "Produto não encontrado.";
   }
-} else {
-  echo "Nenhum produto encontrado.";
-}
 
-// Fecha a conexão com o banco de dados
-$conn->close();
+  // Fecha a conexão com o banco de dados
+  $conn->close();
+} else {
+  echo "ID do produto não fornecido na URL.";
+}
 ?>
