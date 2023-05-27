5382fca5a666434a1979f5817c23e6006dd76729
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-27 15:30:23 -0300

    correção do footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-<footer class="py-6">
-  <div class="container mx-auto flex items-center justify-between bg-green-500">
-    <div class="flex-col md:flex-row items-center">
-      <img class="order-last md:order-first" src="img/Ativo 1 black.png" alt="Logo">
-      <p class="text-2xl font-bold text-red-500">Criando soluções digitais.</p>
+<footer class="py-6 bg-green-500">
+  <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+    <div class="flex items-center">
+      <img class="order-last md:order-first w-16" src="img/Ativo 1 black.png" alt="Logo">
+      <p class="text-2xl font-bold text-red-500 ml-4">Criando soluções digitais.</p>
     </div>
-    <div>
+    <div class="mt-6 md:mt-0">
       <h3 class="text-gray-100 font-bold mb-3">Contato</h3>
       <ul class="text-gray-100">
         <!-- <li class="mb-2">Endereço: Rua X, 123, São Paulo, SP</li> -->
@@ -12,7 +12,7 @@
         <li>Email: contato@meusite.com.br</li>
       </ul>
     </div>
-    <div>
+    <div class="mt-6 md:mt-0">
       <h3 class="text-gray-100 font-bold mb-3">Redes Sociais</h3>
       <ul class="flex items-center">
         <li class="mr-4">
@@ -34,4 +34,3 @@
     </div>
   </div>
 </footer>
-</html>
