7f929b6318ab15d8a6193d8bb12900ea7663f719
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-27 11:59:42 -0300

    .

--- produto.php
+++ produto.php
@@ -30,12 +30,17 @@ if (isset($_GET['id'])) {
     $preco = $row["preco"];
     ?>
 
-    <div class="max-w-md mx-auto bg-white shadow-md rounded-md p-6">
-      <img src="<?php echo $imagem; ?>" alt="Imagem do Produto" class="w-full mb-4">
+<div class="max-w-md mx-auto bg-white shadow-md rounded-md p-6">
+  <div class="flex">
+    <img src="<?php echo $imagem; ?>" alt="Imagem do Produto" class="w-1/2 mr-4">
+    <div class="w-1/2">
       <h2 class="text-2xl font-bold mb-2"><?php echo $nome; ?></h2>
       <p class="text-gray-600 mb-4"><?php echo $descricao; ?></p>
       <p class="text-lg font-bold">Preço: R$ <?php echo $preco; ?></p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Entrar em contato</button>
     </div>
+  </div>
+</div>
 
     <?php
   } else {
