d9254a0f14e8e55f6f189794e19f37151ae1067d
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-29 13:07:40 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -4,11 +4,12 @@ require_once "dbconn.php";
 function isUserLoggedIn(): bool {
   return isset($_SESSION['login']) && $_SESSION['login'] === true;
 }
+
 // Cabeçalho
 if(isUserLoggedIn()):
-	require_once 'header_loggedin.php';
+  require_once 'header_loggedin.php';
 else:
-	require_once 'header.php';
+  require_once 'header.php';
 endif;
 
 // Verifica se o parâmetro "id" está presente na URL
@@ -17,7 +18,10 @@ if (isset($_GET['id'])) {
   $produto_id = $_GET['id'];
 
   // Faz a requisição ao banco de dados para obter as informações do produto com o ID correspondente
-  $sql = "SELECT imagem, nome, descricao, preco FROM produtos WHERE id = $produto_id";
+  $sql = "SELECT produtos.id AS produto_id, produtos.imagem, produtos.nome, produtos.descricao, produtos.preco, produtos.visualizacoes, usuarios.telefone
+          FROM produtos
+          INNER JOIN usuarios ON produtos.usuario_id = usuarios.id
+          WHERE produtos.id = $produto_id";
   $result = $conn->query($sql);
 
   // Verifica se existe um registro correspondente ao ID
@@ -28,19 +32,35 @@ if (isset($_GET['id'])) {
     $nome = $row["nome"];
     $descricao = $row["descricao"];
     $preco = $row["preco"];
+    $visualizacoes = $row["visualizacoes"];
+    $telefone = $row["telefone"];
+
+    // Incrementa o contador de visualizações
+    $novas_visualizacoes = $visualizacoes + 1;
+    $sql = "UPDATE produtos SET visualizacoes = $novas_visualizacoes WHERE id = $produto_id";
+    $conn->query($sql);
     ?>
 
-<div class="max-w-md mx-auto bg-white shadow-md rounded-md p-6">
-  <div class="flex">
-    <img src="<?php echo $imagem; ?>" alt="Imagem do Produto" class="w-1/2 mr-4">
-    <div class="w-1/2">
-      <h2 class="text-2xl font-bold mb-2"><?php echo $nome; ?></h2>
-      <p class="text-gray-600 mb-4"><?php echo $descricao; ?></p>
-      <p class="text-lg font-bold">Preço: R$ <?php echo $preco; ?></p>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Entrar em contato</button>
+    <div class="max-w-md mx-auto bg-white shadow-md rounded-md p-6">
+      <div class="flex">
+        <img src="<?php echo $imagem; ?>" alt="Imagem do Produto" class="w-1/2 mr-4">
+        <div class="w-1/2">
+          <h2 class="text-2xl font-bold mb-2"><?php echo $nome; ?></h2>
+          <p class="text-gray-600 mb-4"><?php echo $descricao; ?></p>
+          <p class="text-lg font-bold">Preço: R$ <?php echo $preco; ?></p>
+          <?php if (isUserLoggedIn()): ?>
+            <p class="text-lg font-bold">Telefone do Vendedor: <?php echo $telefone; ?></p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Entrar em contato</button>
+          <?php else: ?>
+            <span>Para realizar essa ação, é necessário estar logado.</span>
+            <div class="mt-4">
+              <a href="cadastro.php" class="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Cadastre-se</a>
+              <a href="login.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">Login</a>
+            </div>
+          <?php endif; ?>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
     <?php
   } else {
