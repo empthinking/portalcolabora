64b806bd9dded4cc67362a992ce52faf26c37c12
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-29 15:48:52 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -37,15 +37,17 @@ if (isset($_GET['id'])) {
     $sql = "UPDATE produtos SET visualizacoes = $novas_visualizacoes WHERE id = $produto_id";
     $conn->query($sql);
 
-    // Registra o histórico de acesso
-    if (isUserLoggedIn()) {
-      $usuario_id = $_SESSION['id'];
-      $produto_id = mysqli_real_escape_string($conn, $id);
-      $query = "INSERT INTO historico (usuario_id, produto_id) VALUES ('$usuario_id', '$produto_id')";
-      $result = mysqli_query($conn, $query);
-      if (!$result) {
-        throw new Exception("Erro ao registrar histórico: " . mysqli_error($conn));
-      }
+// Registra o histórico de acesso
+if (isUserLoggedIn()) {
+  $usuario_id_logado = $_SESSION['id'];
+  $produto_id = mysqli_real_escape_string($conn, $produto_id);
+  $query = "INSERT INTO historico (usuario_id, produto_id) VALUES ('$usuario_id_logado', '$produto_id')";
+  $result = mysqli_query($conn, $query);
+  if (!$result) {
+    throw new Exception("Erro ao registrar histórico: " . mysqli_error($conn));
+  }
+}
+
     }
 
     ?>
