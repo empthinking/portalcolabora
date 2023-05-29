750d8824c54fe57ec9036ce2dc105d1822e2121b
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-29 10:26:10 -0300

    att

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
                             class="flex-shrink-0">
                     </a>
                 </div>
-                <!-- <div class="flex  navbar-toggle md:hidden">
+                <div class="flex  navbar-toggle md:hidden">
                     <button type="button"
                         class="navbarToggle text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
                         aria-label="toggle menu">
@@ -38,9 +38,7 @@
                                 d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 4H4v2h16v-2z"></path>
                         </svg>
                     </button>
-                </div> -->
-            </div>
-            <div class="navbar-menu md:flex items-center hidden">
+                </div>
                 <div class="flex flex-col md:flex-row md:mx-6">
                     <a class="my-1 text-gray-700 font-medium md:mx-4 md:my-0 hover:text-gray-900"
                         href="index.php">Início</a>
@@ -48,88 +46,89 @@
                         href="contato.php">Contato</a>
                     <a class="my-1 text-gray-700 font-medium md:mx-4 md:my-0 hover:text-gray-900" href="faq.php">FAQ</a>
                 </div>
-                <div class="flex items-center py-2 -mx-4 md:mx-4">
-                    <form method="post" action="pesquisa.php">
-                        <div class="relative mr-10 md:mx-0">
-                            <input
-                                class="bg-gray-200 rounded-full border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 pl-4 pr-10 py-2 w-40"
-                                type="text" name="search" placeholder="Pesquisar...">
-                            <button type="submit" class="absolute right-0 mt-2 mr-2">
-                                <span class="material-symbols-outlined">search</span>
-                            </button>
-                        </div>
-                    </form>
-                </div>
+            </div>
+            <div class="flex items-center py-2 -mx-4 md:mx-4">
+                <form method="post" action="pesquisa.php">
+                    <div class="relative mr-10 md:mx-0">
+                        <input
+                            class="bg-gray-200 rounded-full border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 pl-4 pr-10 py-2 w-40"
+                            type="text" name="search" placeholder="Pesquisar...">
+                        <button type="submit" class="absolute right-0 mt-2 mr-2">
+                            <span class="material-symbols-outlined">search</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
 
-                <div class="flex items-center ml-5">
-                    <a href="cadastro.php"><button
-                            class="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full ml-4">Cadastrar</button></a>
-                    <button class="bg-green-200 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full ml-2"
-                        onclick="document.getElementById('singIn').style.display='block'">Entrar</button>
-                    <div id="singIn" class="modal hidden fixed z-10 inset-0 overflow-y-auto">
-                        <div class="flex items-center justify-center min-h-screen menu-overlay absolute inset-0 bg-gray-900"
-                            style="opacity: 0.9;">
-                            <div class="bg-white rounded-lg w-full max-w-md mx-auto p-8">
-                                <h1 class="text-3xl font-bold mb-8 text-center">Login</h1>
-                                <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-                                    <div class="mb-4 form-group">
-                                        <label class="block font-bold mb-2" for="email">
-                                            Email
-                                        </label>
-                                        <input
-                                            class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                                            type="email" placeholder="exemplo@exemplo.com" id="email" name="email">
-                                    </div>
-                                    <div class="mb-6 form-group">
-                                        <label class="block font-bold mb-2" for="password">
-                                            Senha
-                                        </label>
-                                        <input
-                                            class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                                            type="password" id="senha" name="password">
-                                        <i class="fa fa-eye" aria-hidden="true"></i>
-                                    </div>
-                                    <div class="flex items-center justify-between">
-                                        <button type="submit"
-                                            class=" bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                            type="button">
-                                            Entrar
-                                        </button>
-                                        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                                            href="/B/adm/login_admin.php">entrar como admin </a>
-                                    </div>
-                                </form>
-                                <button type="button" onclick="document.getElementById('singIn').style.display='none'"
-                                    class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mt-4 w-full">
-                                    Cancelar
-                                </button>
-                            </div>
+            <div class="flex items-center ml-5">
+                <a href="cadastro.php"><button
+                        class="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full ml-4">Cadastrar</button></a>
+                <button class="bg-green-200 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full ml-2"
+                    onclick="document.getElementById('singIn').style.display='block'">Entrar</button>
+                <div id="singIn" class="modal hidden fixed z-10 inset-0 overflow-y-auto">
+                    <div class="flex items-center justify-center min-h-screen menu-overlay absolute inset-0 bg-gray-900"
+                        style="opacity: 0.9;">
+                        <div class="bg-white rounded-lg w-full max-w-md mx-auto p-8">
+                            <h1 class="text-3xl font-bold mb-8 text-center">Login</h1>
+                            <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                                <div class="mb-4 form-group">
+                                    <label class="block font-bold mb-2" for="email">
+                                        Email
+                                    </label>
+                                    <input
+                                        class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                                        type="email" placeholder="exemplo@exemplo.com" id="email" name="email">
+                                </div>
+                                <div class="mb-6 form-group">
+                                    <label class="block font-bold mb-2" for="password">
+                                        Senha
+                                    </label>
+                                    <input
+                                        class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                                        type="password" id="senha" name="password">
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <button type="submit"
+                                        class=" bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                        type="button">
+                                        Entrar
+                                    </button>
+                                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                                        href="/B/adm/login_admin.php">entrar como admin </a>
+                                </div>
+                            </form>
+                            <button type="button" onclick="document.getElementById('singIn').style.display='none'"
+                                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mt-4 w-full">
+                                Cancelar
+                            </button>
                         </div>
                     </div>
                 </div>
-                <script>
-                function toggleDropdown() {
-                    var dropdownMenu = document.getElementById("dropdown-menu");
-                    dropdownMenu.classList.toggle("hidden");
-                }
-                var elementos = document.getElementsByClassName('navbar');
-                var el = elementos[0];
-                el.style.backgroundColor = '#63f253';
-
-
-
-
-                btn.addEventListener('click', () => {
-                    let inputSenha = document.querySelector('#senha')
-
-                    if (inputSenha.getAttribute('type') == 'password') {
-                        inputSenha.setAttribute('type', 'text')
-                    } else {
-                        inputSenha.setAttribute('type', 'password')
-                    }
-                })
-                </script>
             </div>
+            <script>
+            function toggleDropdown() {
+                var dropdownMenu = document.getElementById("dropdown-menu");
+                dropdownMenu.classList.toggle("hidden");
+            }
+            var elementos = document.getElementsByClassName('navbar');
+            var el = elementos[0];
+            el.style.backgroundColor = '#63f253';
+
+
+
+
+            btn.addEventListener('click', () => {
+                let inputSenha = document.querySelector('#senha')
+
+                if (inputSenha.getAttribute('type') == 'password') {
+                    inputSenha.setAttribute('type', 'text')
+                } else {
+                    inputSenha.setAttribute('type', 'password')
+                }
+            })
+            </script>
+        </div>
         </div>
     </nav>
     <script>
