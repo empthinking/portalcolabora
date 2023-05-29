d8a3c7226023a8c68c92cc972d42e7f9e28f6488
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-29 09:17:49 -0300

    att

--- footer.php
+++ footer.php
@@ -1,36 +1,36 @@
 <footer class="py-6 bg-green-500">
-  <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
-    <div class="flex items-center">
-      <img class="order-last md:order-first w-16" src="img/Ativo 1 black.png" alt="Logo">
-      <p class="text-2xl font-bold text-red-500 ml-4">Criando soluções digitais.</p>
+    <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div class="flex items-center">
+            <img class="order-last md:order-first w-16" src="img/Ativo 1 black.png" alt="Logo">
+            <p class="text-2xl font-bold text-red-500 ml-4">Criando soluções digitais.</p>
+        </div>
+        <div class="mt-6 md:mt-0">
+            <h3 class="text-gray-100 font-bold mb-3">Contato</h3>
+            <ul class="text-gray-100">
+                <!-- <li class="mb-2">Endereço: Rua X, 123, São Paulo, SP</li> -->
+                <li class="mb-2">Telefone: (91) XXXX-XXXX</li>
+                <li>Email: contato@meusite.com.br</li>
+            </ul>
+        </div>
+        <div class="mt-6 md:mt-0">
+            <h3 class="text-gray-100 font-bold mb-3">Redes Sociais</h3>
+            <ul class="flex items-center">
+                <li class="mr-4">
+                    <a href="#">
+                        <i class="fab fa-facebook text-gray-100 text-lg"></i>
+                    </a>
+                </li>
+                <li class="mr-4">
+                    <a href="#">
+                        <i class="fab fa-twitter text-gray-100 text-lg"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-instagram text-gray-100 text-lg"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div class="mt-6 md:mt-0">
-      <h3 class="text-gray-100 font-bold mb-3">Contato</h3>
-      <ul class="text-gray-100">
-        <!-- <li class="mb-2">Endereço: Rua X, 123, São Paulo, SP</li> -->
-        <li class="mb-2">Telefone: (91) XXXX-XXXX</li>
-        <li>Email: contato@meusite.com.br</li>
-      </ul>
-    </div>
-    <div class="mt-6 md:mt-0">
-      <h3 class="text-gray-100 font-bold mb-3">Redes Sociais</h3>
-      <ul class="flex items-center">
-        <li class="mr-4">
-          <a href="#">
-            <i class="fab fa-facebook text-gray-100 text-lg"></i>
-          </a>
-        </li>
-        <li class="mr-4">
-          <a href="#">
-            <i class="fab fa-twitter text-gray-100 text-lg"></i>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class="fab fa-instagram text-gray-100 text-lg"></i>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
 </footer>
