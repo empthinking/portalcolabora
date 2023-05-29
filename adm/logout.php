fcaea8ee1de3c8de11dd4435bbf89582211d992a
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-29 11:04:06 -0300

    Update logout.php

--- adm/logout.php
+++ adm/logout.php
@@ -4,7 +4,11 @@ require_once "dbconn.php";
 
 // Logout do administrador
 if (isset($_GET["logout"])) {
+    // Encerra a sessão
+    session_unset();
     session_destroy();
+
+    // Redireciona para a página inicial
     header("Location: index.php");
     exit();
 }
@@ -21,7 +25,7 @@ if (isset($_GET["logout"])) {
             <h2 class="text-2xl font-bold mb-4">Logout do Administrador</h2>
             <p>Deseja realmente fazer logout como administrador?</p>
             <div class="flex justify-between mt-4">
-                <a href="login_admin.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</a>
+                <a href="?logout" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</a>
                 <a href="area_admin.php" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">Cancelar</a>
             </div>
         </div>
