c3a228a29c055bd4757c5570b2999471e7a3456e
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-30 18:14:52 -0300

    Update mensagem_recebidas.php

--- mensagem_recebidas.php
+++ mensagem_recebidas.php
@@ -19,8 +19,6 @@ if (isset($_SESSION['id'])) {
     }
 }
 ?>
-
-    <style>
         .message-card {
             border: 1px solid #ccc;
             padding: 10px;
@@ -28,17 +26,19 @@ if (isset($_SESSION['id'])) {
         }
     </style>
 </head>
-<body>
-    <h1>Lista de Mensagens</h1>
+<body class="bg-gray-100">
+    <div class="container mx-auto py-8">
+        <h1 class="text-2xl font-bold mb-4">Lista de Mensagens</h1>
 
-    <?php if (isset($mensagens) && count($mensagens) > 0): ?>
-        <?php foreach ($mensagens as $mensagem): ?>
-            <div class="message-card">
-                <p>Remetente: <?php echo $mensagem['remetente']; ?></p>
-                <p>Destinatário: <?php echo $mensagem['destinatario']; ?></p>
-                <p>Mensagem: <?php echo $mensagem['mensagem']; ?></p>
-            </div>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <p>Nenhuma mensagem encontrada para este destinatário.</p>
-    <?php endif; ?>
+        <?php if (isset($mensagens) && count($mensagens) > 0): ?>
+            <?php foreach ($mensagens as $mensagem): ?>
+                <div class="message-card bg-white rounded p-4 mb-4">
+                    <p class="mb-2"><span class="font-semibold">Remetente:</span> <?php echo $mensagem['remetente']; ?></p>
+                    <p class="mb-2"><span class="font-semibold">Destinatário:</span> <?php echo $mensagem['destinatario']; ?></p>
+                    <p class="mb-2"><span class="font-semibold">Mensagem:</span> <?php echo $mensagem['mensagem']; ?></p>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p class="text-gray-500">Nenhuma mensagem encontrada para este destinatário.</p>
+        <?php endif; ?>
+    </div>
