6198ee3e5ca0b8e49911a7264ad9e08a0414854e
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-30 18:17:25 -0300

    Update mensagem_recebidas.php

--- mensagem_recebidas.php
+++ mensagem_recebidas.php
@@ -10,19 +10,38 @@ if (isset($_SESSION['id'])) {
     // Array para armazenar as mensagens
     $mensagens = array();
 
+    // Contador de mensagens não lidas
+    $mensagensNaoLidas = 0;
+
     // Verifica se existem mensagens
     if ($result->num_rows > 0) {
         // Loop através dos resultados do banco de dados
         while ($row = $result->fetch_assoc()) {
             $mensagens[] = $row;
+
+            // Verifica se a mensagem não foi lida
+            if ($row['lida'] == 0) {
+                $mensagensNaoLidas++;
+            }
         }
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exibir Mensagens</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    <style>
         .message-card {
             border: 1px solid #ccc;
             padding: 10px;
             margin-bottom: 10px;
+        }
+
+        .message-card.unread {
+            font-weight: bold;
         }
     </style>
 </head>
@@ -32,13 +51,29 @@ if (isset($_SESSION['id'])) {
 
         <?php if (isset($mensagens) && count($mensagens) > 0): ?>
             <?php foreach ($mensagens as $mensagem): ?>
-                <div class="message-card bg-white rounded p-4 mb-4">
+                <div class="message-card <?php echo $mensagem['lida'] == 0 ? 'unread' : ''; ?> bg-white rounded p-4 mb-4">
                     <p class="mb-2"><span class="font-semibold">Remetente:</span> <?php echo $mensagem['remetente']; ?></p>
                     <p class="mb-2"><span class="font-semibold">Destinatário:</span> <?php echo $mensagem['destinatario']; ?></p>
                     <p class="mb-2"><span class="font-semibold">Mensagem:</span> <?php echo $mensagem['mensagem']; ?></p>
+                    <button class="text-red-500 font-semibold mt-2" onclick="apagarMensagem(<?php echo $mensagem['id']; ?>)">Apagar mensagem</button>
                 </div>
             <?php endforeach; ?>
         <?php else: ?>
             <p class="text-gray-500">Nenhuma mensagem encontrada para este destinatário.</p>
         <?php endif; ?>
+
+        <?php if ($mensagensNaoLidas > 0): ?>
+            <p class="mt-4">Mensagens não lidas: <?php echo $mensagensNaoLidas; ?></p>
+        <?php else: ?>
+            <p class="mt-4">Todas as mensagens foram lidas.</p>
+        <?php endif; ?>
     </div>
+
+    <script>
+        function apagarMensagem(id) {
+            // Aqui você pode adicionar a lógica para apagar a mensagem com o ID fornecido
+            // por exemplo, fazendo uma requisição AJAX para um script PHP que realiza a exclusão no banco de dados.
+            // Após apagar a mensagem, você pode recarregar a página ou atualizar a lista de mensagens.
+        }
+    </script>
+
