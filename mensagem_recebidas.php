ff77a12af4be42f33519a80d374f98f4354ffa23
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-05-30 18:04:51 -0300

    Update mensagem_recebidas.php

--- mensagem_recebidas.php
+++ mensagem_recebidas.php
@@ -1,66 +1,44 @@
 <?php
-// Atualiza o status de leitura das mensagens
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $mensagem_id = $_POST['mensagem_id'];
-  $usuario_id = $_SESSION['id'];
-
-  $sql = "UPDATE contatos SET lido = 1 WHERE id = $mensagem_id AND destinatario = $usuario_id";
-
-  if ($conn->query($sql) === true) {
-    echo "Status de leitura atualizado.";
-  } else {
-    echo "Erro ao atualizar o status de leitura: " . $conn->error;
-  }
-
-  // Fecha a conexão com o banco de dados
-  $conn->close();
+// Verifica se houve erro na conexão
+if ($conn->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Consulta as mensagens recebidas
-$usuario_id = $_SESSION['id'];
+// Verifica se o destinatário está definido
+if (isset($_SESSION['id'])) {
+    $destinatario = $_SESSION['id'];
 
-$sql = "SELECT c.id, c.remetente, c.mensagem, c.lido, p.nome, p.imagem
-        FROM contatos c
-        INNER JOIN produtos p ON c.produto_id = p.id
-        WHERE c.destinatario = $usuario_id
-        ORDER BY c.id DESC";
+    // Recuperar as mensagens do banco de dados para o destinatário
+    $sql = "SELECT * FROM contatos WHERE destinatario = $destinatario";
+    $result = $conn->query($sql);
 
-$result = $conn->query($sql);
+    // Array para armazenar as mensagens
+    $mensagens = array();
 
-if ($result->num_rows > 0) {
-  while ($row = $result->fetch_assoc()) {
-    $mensagem_id = $row['id'];
-    $remetente = $row['remetente'];
-    $mensagem = $row['mensagem'];
-    $lido = $row['lido'];
-    $produto_nome = $row['nome'];
-    $produto_imagem = $row['imagem'];
-
-    // Formata o status de leitura
-    $status_leitura = $lido ? "Lido" : "<strong>Não lido</strong>";
-
-    // Exibe as informações da mensagem
-    echo "<div>";
-    echo "<h3>Remetente: $remetente</h3>";
-    echo "<p>Produto: $produto_nome</p>";
-    echo "<img src=\"$produto_imagem\" alt=\"Imagem do Produto\" width=\"100\">";
-    echo "<p>$mensagem</p>";
-    echo "<p>Status de leitura: $status_leitura</p>";
-
-    // Botão para marcar a mensagem como lida
-    if (!$lido) {
-      echo "<form method=\"POST\" action=\"mensagem_recebidas.php\">";
-      echo "<input type=\"hidden\" name=\"mensagem_id\" value=\"$mensagem_id\">";
-      echo "<button type=\"submit\">Marcar como lido</button>";
-      echo "</form>";
+    // Verifica se existem mensagens
+    if ($result->num_rows > 0) {
+        // Loop através dos resultados do banco de dados
+        while ($row = $result->fetch_assoc()) {
+            $mensagens[] = $row;
+        }
     }
-
-    echo "</div>";
-  }
-} else {
-  echo "Nenhuma mensagem recebida.";
 }
-
-// Fecha a conexão com o banco de dados
-$conn->close();
 ?>
+    <?php if (isset($mensagens) && count($mensagens) > 0): ?>
+        <?php foreach ($mensagens as $mensagem): ?>
+            <div class="message-card">
+                <p>Remetente: <?php echo $mensagem['remetente']; ?></p>
+                <p>Destinatário: <?php echo $mensagem['destinatario']; ?></p>
+                <p>Mensagem: <?php echo $mensagem['mensagem']; ?></p>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>Nenhuma mensagem encontrada para este destinatário.</p>
+    <?php endif; ?>
+
+    <?php
+    // Fecha a conexão com o banco de dados
+    $conn->close();
+    ?>
+</body>
+</html>
