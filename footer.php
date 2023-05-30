1830bd05292f8171b1c2358a14ce195fcfec54a4
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-05-30 09:09:44 -0300

    atualização do footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="p-5..." style="background-color: #05f21d;">
+<footer class="p-5...">
     <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
         <div class="flex flex-col items-center">
             <img class="order-last md:order-first w-16" src="img/Ativo 1 black.png" alt="Logo">
@@ -8,8 +8,8 @@
             <h3 class="text-gray-100 font-bold mb-3">Contato</h3>
             <ul class="text-gray-100">
                 <!-- <li class="mb-2">Endereço: Rua X, 123, São Paulo, SP</li> -->
-                <li class="mb-2">Telefone: (91) XXXX-XXXX</li>
-                <li>Email: contato@meusite.com.br</li>
+                <li class="mb-2">Telefone: (91) 984388103/li>
+                <li>Email: suporte@portalcolabora.com.br</li>
             </ul>
         </div>
         <div class="mt-6 md:mt-0">
