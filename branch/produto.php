71ab815d9f9d93956afbfae7db7b92bb6ee8da3b
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-07 10:49:25 -0300

    test

--- branch/produto.php
+++ branch/produto.php
@@ -61,7 +61,7 @@ $stmt->close();
                 <p class="card-text">Preço: <?php echo $product_price; ?></p>
                 <p class="card-text">Anunciante: <?php echo $vendor_name; ?></p>
                 <a class="btn btn-success" href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) . "?id=$product_id&mode=register"; ?>">Contatar<a>
-                <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=XXXXXXXXXXX&text=Olá, tudo bem?" target="_blank">Enviar mensagem pelo WhatsApp </a>
+                <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=+55<?php echo $User_Number; ?>&text=Olá, tudo bem?" target="_blank">Enviar mensagem pelo WhatsApp </a>
             </div>
         </div>
 
