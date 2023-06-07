f8cf8b8c6b83545bf7fc3385b923f69bcc7e9872
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-07 16:54:55 -0300

    Update excluirproduto.php

--- excluirproduto.php
+++ excluirproduto.php
@@ -1,80 +1,79 @@
 <?php
-require_once 'db.php';
-
 session_start();
 
-if (!isset($_SESSION['id']) || $_SESSION['type'] !== 'vendedor') {
-    header("Location: login.php");
+// Verificar se o usuário está logado
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: login_admin.php");
     exit();
 }
 
-// Verifica se o ID do produto está presente
+require_once "db.php";
+
+
+// Verificar se o parâmetro 'id' está presente na URL
 if (!isset($_GET['id'])) {
-    header("Location: meusprodutos.php");
+    header("Location: listar_produtos.php");
     exit();
 }
 
-$productId = $_GET['id'];
-$userId = $_SESSION['id'];
+$product_id = $_GET['id'];
 
-// Verifica se o produto pertence ao usuário logado
-$stmt = $db->prepare('SELECT * FROM Products WHERE Product_Id = ? AND User_Id = ?');
-$stmt->bind_param('ii', $productId, $userId);
-$stmt->execute();
-$result = $stmt->get_result();
+// Verificar se o formulário foi enviado para atualizar o produto
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Obter os dados do formulário
+    $product_name = mysqli_real_escape_string($conn, $_POST["product_name"]);
+    $product_description = mysqli_real_escape_string($conn, $_POST["product_description"]);
+    $product_price = mysqli_real_escape_string($conn, $_POST["product_price"]);
 
+    // Atualizar as informações do produto no banco de dados
+    $update_query = "UPDATE Products SET Product_Name = '$product_name', Product_Description = '$product_description', Product_Price = '$product_price' WHERE Product_Id = $product_id";
+    mysqli_query($conn, $update_query);
 
-$stmt->close();
-
-// Exclui o produto do banco de dados
-$stmt = $db->prepare('DELETE FROM Products WHERE Product_Id = ?');
-$stmt->bind_param('i', $productId);
-$stmt->execute();
-
-$stmt = $db->prepare('DELETE FROM Images WHERE Product_Id = ?');
-$stmt->bind_param('i', $productId);
-$stmt->execute();
-
-$stmt->close();
-
-// Remove o diretório das imagens relacionadas ao produto
-$directoryPath = "img/product_images/$productId/";
-if (is_dir($directoryPath)) {
-    $files = array_diff(scandir($directoryPath), array('.', '..'));
-    
-    foreach ($files as $file) {
-        $filePath = $directoryPath . $file;
-        
-        if (is_dir($filePath)) {
-            deleteDirectory($filePath);
-        } else {
-            unlink($filePath);
-        }
-    }
-
-    rmdir($directoryPath);
+    // Redirecionar de volta para a página listar_produtos.php após a atualização
+    header("Location: listar_produtos.php");
+    exit();
 }
 
-header("Location: meusprodutos.php");
-exit();
+// Consultar o banco de dados para obter as informações do produto
+$query = "SELECT * FROM Products WHERE Product_Id = $product_id";
+$result = mysqli_query($conn, $query);
+$product = mysqli_fetch_assoc($result);
 
-function deleteDirectory(string $directoryPath): bool {
-    if (!is_dir($directoryPath)) {
-        return false;
-    }
-
-    $files = array_diff(scandir($directoryPath), array('.', '..'));
-    
-    foreach ($files as $file) {
-        $filePath = $directoryPath . $file;
-        
-        if (is_dir($filePath)) {
-            deleteDirectory($filePath);
-        } else {
-            unlink($filePath);
-        }
-    }
-
-    return rmdir($directoryPath);
+// Verificar se o produto foi encontrado
+if (!$product) {
+    header("Location: listar_produtos.php");
+    exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Editar Produto</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body class="bg-gray-200">
+    <div class="container mt-5">
+        <h2>Editar Produto</h2>
+        <form method="POST">
+            <div class="form-group">
+                <label for="product_name">Nome do Produto:</label>
+                <input type="text" class="form-control" id="product_name" name="product_name" value="<?php echo $product['Product_Name']; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="product_description">Descrição do Produto:</label>
+                <textarea class="form-control" id="product_description" name="product_description"><?php echo $product['Product_Description']; ?></textarea>
+            </div>
+            <div class="form-group">
+                <label for="product_price">Preço do Produto:</label>
+                <input type="text" class="form-control" id="product_price" name="product_price" value="<?php echo $product['Product_Price']; ?>" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Atualizar</button>
+            <a href="listar_produtos.php" class="btn btn-secondary">Cancelar</a>
+        </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
