bfc842b55024f10d0e9820f2f9c54815a304c8f2
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-07 15:31:40 -0300

    Update login.php

--- branch/login.php
+++ branch/login.php
@@ -72,7 +72,7 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
     <div class="fundo">
         <div class="container mt-5">
             <div class="row justify-content-center">
-                <div class="col-md-6">
+                <div class="col-md-6 bg-white rounded-lg w-full max-w-md mx-auto p-8"">
                     <div class="text-center">
                         <img class="mx-auto" src="img/logo.png" width="323px" alt="logo">
                     </div>
