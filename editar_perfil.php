6d13f4b1222a816d693405ea462281de2fef77c4
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-07 17:27:28 -0300

    Update editar_perfil.php

--- editar_perfil.php
+++ editar_perfil.php
@@ -2,7 +2,30 @@
 require_once 'db.php';
 
 session_start();
-?><!DOCTYPE html>
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verifique se os campos do formulário estão definidos
+    if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['numero'])) {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $numero = $_POST['numero'];
+
+        // Recupere o ID do usuário logado
+        $userId = $_SESSION['id'];
+
+        // Atualize os dados do usuário no banco de dados
+        $stmt = $db->prepare('UPDATE Users SET Nome = ?, Email = ?, Numero = ? WHERE User_Id = ?');
+        $stmt->bind_param('sssi', $nome, $email, $numero, $userId);
+        $stmt->execute();
+        $stmt->close();
+
+        // Redirecione para a página de perfil
+        header("Location: perfil.php");
+        exit();
+    }
+}
+?>
+<!DOCTYPE html>
 <html>
 <head>
   <title>Editar Perfil</title>
