e5e5cd2cb2d22ac2d83df51abdab130c8ed7604f
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-07 11:20:34 -0300

    Create logout.php

---
+++ branch/admin/logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+// Verifica se a sessão do admin está ativa
+if (isset($_SESSION['admin']) && $_SESSION['admin'] === true) {
+    // Encerra a sessão do admin
+    $_SESSION['admin'] = false;
+    session_unset();
+    session_destroy();
+}
+
+// Redireciona para a página de login do admin
+header("Location: ./index.php");
+exit();
+?>
