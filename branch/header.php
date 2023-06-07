735a33fbdd8136c326944f940403e1bf07c31a28
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-07 10:20:50 -0300

    iclusão da api de ícones

--- branch/header.php
+++ branch/header.php
@@ -28,6 +28,7 @@ $checkUnreadMessages = function() use ($id, $db) : bool {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PortalColabora</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <style>
     .bg-header {
       background-color: rgb(99, 242, 83);
