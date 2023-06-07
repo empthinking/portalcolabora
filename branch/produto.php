32f2743f4337d44dc9ad4b0296a19fd8bb05bc84
empthinking <117464630+empthinking@users.noreply.github.com>
2023-06-07 11:40:19 -0300

    Update produto.php

--- branch/produto.php
+++ branch/produto.php
@@ -60,10 +60,8 @@ $stmt->close();
                 <p class="card-text"><?php echo $product_description; ?></p>
                 <p class="card-text">Preço: <?php echo $product_price; ?></p>
                 <p class="card-text">Anunciante: <?php echo $vendor_name; ?></p>
-                <?php if(isUserLoggedIn()): ?>
-                <a class="btn btn-success" href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) . "?id=$product_id&mode=register"; ?>">Contatar<a>
-                <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=+55<?php echo $User_Number; ?>&text=Olá, tudo bem?" target="_blank">Enviar mensagem pelo WhatsApp </a>
-                <?php endif; ?>
+                <a class="btn btn-success" href="<?php echo echo isUserLoggedIn() ? : htmlspecialchars($_SERVER['PHP_SELF']) . "?id=$product_id&mode=register" : "login.php"; ?>">Contatar<a>
+                <a class="btn btn-success" href="<?php echo isUserLoggedIn() ? "https://api.whatsapp.com/send?phone=+55$User_Number&text=Olá, tudo bem?" : "login.php"; ?>target="_blank">Enviar mensagem pelo WhatsApp </a>
             </div>
         </div>
 
