7ce45731060823a907cc5ca6daeaea699385ad94
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-07 12:10:08 -0300

    indentação

--- branch/login.php
+++ branch/login.php
@@ -57,13 +57,14 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
         display: flex;
         flex-direction: column;
     }
+
     .fundo {
         background-image: url('img/Fundo\ login.jpeg');
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center;
         height: 100vh;
-    }    
+    }
     </style>
 </head>
 
@@ -73,7 +74,7 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
             <div class="row justify-content-center">
                 <div class="col-md-6">
                     <div class="text-center">
-                    <img class="mx-auto" src="img/logo G (2).png" width="323px" alt="logo">
+                        <img class="mx-auto" src="img/logo G (2).png" width="323px" alt="logo">
                     </div>
                     <h2 class="mb-4 text-center">LOGIN</h2>
                     <h4 class="text-danger"><?php echo $error; ?></h4>
