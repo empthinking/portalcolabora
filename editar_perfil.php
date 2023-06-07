97c672200714556957f9fa886e2d2e51f7956d57
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-07 16:35:46 -0300

    Update editar_perfil.php

--- editar_perfil.php
+++ editar_perfil.php
@@ -2,64 +2,38 @@
 require_once 'db.php';
 
 session_start();
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html>
-
 <head>
-    <title>Editar Perfil</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <style>
-        .bg-header {
-            background-color: rgb(99, 242, 83);
-        }
-
-        body {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-        }
-
-        .fundo {
-            background-image: url('img/Fundo\ login.jpeg');
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center;
-            height: 100vh;
-        }
-    </style>
+  <title>Editar Perfil</title>
+  <link rel="stylesheet" href="style.css"> <!-- Arquivo CSS para estilização -->
 </head>
-
 <body>
-    <div class="fundo">
-        <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="text-center">
-                        <img class="mx-auto" src="img/logo.png" width="323px" alt="logo">
-                    </div>
-                    <h2 class="mb-4 text-center">Editar Perfil</h2>
-                    <h4 class="text-danger"><?php echo $error; ?></h4>
-                    <form action="<?php echo $url; ?>" method="POST">
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required
-                                value="<?php echo $user['User_Email']; ?>"
-                                pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Nova Senha</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </div>
-                        <div class="d-flex">
-                            <button type="submit" class="btn btn-primary m-3">Salvar</button>
-                            <a href="index.php" class="btn btn-danger m-3">Voltar</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+  <h1>Editar Perfil</h1>
+
+  <form action="atualizar_perfil.php" method="POST">
+    <div class="form-group">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" name="nome" value="Nome atual" required>
     </div>
+
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" value="email@example.com" required>
+    </div>
+
+    <div class="form-group">
+      <label for="numero">Número:</label>
+      <input type="tel" id="numero" name="numero" value="123456789" required>
+    </div>
+
+    <button type="submit">Salvar</button>
+  </form>
+
+  <script src="script.js"></script> <!-- Arquivo JavaScript para interatividade -->
+</body>
+</html>
+
     <?php
 require_once 'footer.php';
 ?>
