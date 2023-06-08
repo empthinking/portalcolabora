2f747b70bde89390f679cdb20f03305909738655
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-08 00:37:32 -0300

    att

--- sobrenos.php
+++ sobrenos.php
@@ -1,10 +1,10 @@
 <?php 
 require_once "header.php";
 ?>
+<main>
 <div class="bg-primary text-white text-center py-5">
     <h1>Sobre Nós</h1>
 </div>
-
 <div class="container py-5">
     <div class="row">
         <div class="col-md-6">
@@ -21,6 +21,7 @@ require_once "header.php";
         </div>
     </div>
 </div>
+</main>
 <?php 
 require_once "footer.php";
 ?>
