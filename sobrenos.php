b5fad4e8da7cf9949dee864af2df881ca2a1dc87
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-10 16:43:42 -0300

    att sobre nos

--- sobrenos.php
+++ sobrenos.php
@@ -1,4 +1,9 @@
-<?php require_once 'header.php';?>
+<?php
+session_start();
+require_once 'db.php';
+require_once 'header.php';
+require_once 'header.php';
+?>
 <html>
 <body>
     <h1>Sobre Nós</h1>
