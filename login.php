5b371710a202700927650919020e1b84cf401772
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-10 23:47:29 -0300

    asd

--- login.php
+++ login.php
@@ -113,7 +113,7 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
                             <input type="password" class="form-control" id="password" name="password"
                                 value="<?php echo $password; ?>" required>
                         </div>
-                        <div class="d-flex">
+                        <div class="d-flex justify-content-center">
                             <button type="submit" class="btn btn-primary m-3">Entrar</button>
                             <a href="index.php" class="btn btn-danger m-3">Voltar</a>
                         </div>
