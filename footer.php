d1850a6ca298e3745dc881b86e807c1ccdd61551
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-11 20:40:49 -0300

    home footer

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-
-  <footer class="py-3 mt-auto">
+<footer class="py-3 mt-auto">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Início</a></li>
-      <li class="nav-item"><a href="contato.php" class="nav-link px-2 text-muted">Contato</a></li>
-      <li class="nav-item"><a href="faq.php" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="sobrenos.php" class="nav-link px-2 text-muted">Sobre Nós</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted"><i class="fas fa-house"></i> Início</a></li>
+        <li class="nav-item"><a href="contato.php" class="nav-link px-2 text-muted">Contato</a></li>
+        <li class="nav-item"><a href="faq.php" class="nav-link px-2 text-muted">FAQs</a></li>
+        <li class="nav-item"><a href="sobrenos.php" class="nav-link px-2 text-muted">Sobre Nós</a></li>
     </ul>
     <p class="text-center text-muted"><i class="far fa-copyright"></i> <?php echo date('Y'); ?> UEPA</p>
-  </footer>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</footer>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
