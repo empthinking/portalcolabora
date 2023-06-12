37ee81987cd597f9b72718d18cf3c98dc0991e4e
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-12 00:15:09 -0300

    ASD

--- register.php
+++ register.php
@@ -60,7 +60,6 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
     <title>User Registration Form</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" type="image/x-icon" href="./img/favicon-32x32.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
@@ -149,6 +148,7 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
             </div>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
     <script>
