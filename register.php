68a3ae798489e9330e2d72e3c621f806d9eeeccb
netowanzeler <106743877+netowanzeler@users.noreply.github.com>
2023-06-12 00:13:20 -0300

    ATT

--- register.php
+++ register.php
@@ -59,6 +59,8 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
 <head>
     <title>User Registration Form</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" type="image/x-icon" href="./img/favicon-32x32.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
