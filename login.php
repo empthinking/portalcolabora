798ca349f8e1c968b28779fb230b87f5ebea7a98
empthinking <117464630+empthinking@users.noreply.github.com>
2023-06-13 15:20:03 -0300

    Atualizar o login.php

--- login.php
+++ login.php
@@ -90,8 +90,8 @@ $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
                                 value="<?php echo $password; ?>" required>
                         </div>
                         <div class="d-flex justify-content-center ">
-                            <button type="submit" class="btn btn-primary btn-lg m-3">Entrar</button>
-                            <a href="index.php" class="btn btn-danger btn-lg m-3">Voltar</a>
+                            <button type="submit" class="btn btn-primary btn-lg m-3"><i class="fa-light fa-right-to-bracket"></i>Entrar</button>
+                            <a href="index.php" class="btn btn-danger btn-lg m-3"><i class="fa-solid fa-arrow-rotate-left"></i>Voltar</a>
                         </div>
                     </form>
                 </div>
