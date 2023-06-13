94beedde7173dec8229445699076b97d68c1766d
arnaldoG42022 <117642004+arnaldoG42022@users.noreply.github.com>
2023-06-13 14:41:03 -0300

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -82,14 +82,13 @@ echo <<<FORM
         </div>
       </div>
       <div class="d-flex flex-column">
-  <button type="button" class="btn btn-success btn-lg" onclick="addImageField()">Adicionar</button>
+  <button type="button" class="btn btn-success btn-lg mb-2" onclick="addImageField()">Adicionar</button>
   <button type="button" class="btn btn-danger btn-lg" onclick="removeImageField()">Remover</button>
-  <div class="d-flex mt-auto">
-    <button type="submit" name="add_product" class="btn btn-primary">Registrar</button>
-    <button type="submit" name="add_product" class="btn btn-danger ml-2" onclick="history.back()"><i class="fas fa-undo"></i> Voltar</button>
-  </div>
 </div>
-</form>
+<div class="container m-3">
+  <button type="submit" name="add_product" class="btn btn-primary">Registrar</button>
+  <button type="submit" name="add_product" class="btn btn-danger mt-2" onclick="history.back()"><i class="fas fa-undo"></i> Voltar</button>
+</div>
   </div>
 FORM;
 ?>
