27001fbb79184dda0d4c254e205c9524446dec7a
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-13 20:57:30 -0300

    Update editarproduto.php

--- editarproduto.php
+++ editarproduto.php
@@ -1,130 +1,112 @@
 <?php
-session_start();
-
 require_once 'db.php';
 
-// Assuming you have already connected to the database and obtained the product ID from the URL
-$product_id = $_GET['id'] ?? '';
-$error = false;
+session_start();
 
-// Query the database to fetch the product information
-$stmt = $db->prepare("
-    SELECT p.Product_Name, p.Product_Description, p.Product_Price, p.Product_Date, u.User_Name, u.User_Id, u.User_Number
-    FROM Products p
-    INNER JOIN Users u ON p.User_Id = u.User_Id
-    WHERE p.Product_Id = ?
-");
-$stmt->bind_param('i', $product_id);
-if ($stmt->execute()) {
-    $stmt->bind_result($product_name, $product_description, $product_price, $product_date, $vendor_name, $vendor_id, $User_Number);
-    $stmt->fetch();
-} else {
-    $error = false;
-}
-$stmt->close();
-
-if (isset($_GET['mode']) && $_GET['mode'] === 'register') {
-    require_once 'send_message.php';
+if (!isset($_SESSION['id']) || $_SESSION['type'] !== 'vendedor') {
+    header("Location: login.php");
     exit();
 }
 
-// Query the database to fetch the associated images
-$stmt = $db->prepare("
-    SELECT Image_Name
-    FROM Images
-    WHERE Product_Id = ?
-");
-$stmt->bind_param('i', $product_id);
-$stmt->execute();
-$stmt->bind_result($image_name);
-
-$images = array();
-
-while ($stmt->fetch()) {
-    $images[] = $image_name;
+// Verifica se o ID do produto está presente
+if (!isset($_GET['id'])) {
+    header("Location: meusprodutos.php");
+    exit();
 }
 
+$productId = $_GET['id'];
+$userId = $_SESSION['id'];
+
+// Verifica se o produto pertence ao usuário logado
+$stmt = $db->prepare('SELECT * FROM Products WHERE Product_Id = ? AND User_Id = ?');
+$stmt->bind_param('ii', $productId, $userId);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 0) {
+    // O produto não pertence ao usuário logado
+    header("Location: meusprodutos.php");
+    exit();
+}
+
+$product = $result->fetch_assoc();
+
 $stmt->close();
+
+// Query the database to fetch the product images
+$stmt = $db->prepare('SELECT * FROM Product_Images WHERE Product_Id = ?');
+$stmt->bind_param('i', $productId);
+$stmt->execute();
+$imageResult = $stmt->get_result();
+$images = $imageResult->fetch_all(MYSQLI_ASSOC);
+
+$stmt->close();
+
+// Verifica se o formulário foi enviado para salvar as alterações
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Obtém os dados do formulário
+    $updatedName = $_POST['name'];
+    $updatedPrice = doubleval($_POST['price']);
+    $updatedDescription = $_POST['description'];
+
+    // Atualiza os dados do produto no banco de dados
+    $stmt = $db->prepare('UPDATE Products SET Product_Name = ?, Product_Price = ?, Product_Description = ? WHERE Product_Id = ? AND User_Id = ?');
+    $stmt->bind_param('ssssi', $updatedName, $updatedPrice, $updatedDescription, $productId, $userId);
+    $stmt->execute();
+    $stmt->close();
+
+    // Redireciona de volta para a página de produtos do usuário
+    header("Location: meusprodutos.php");
+    exit();
+}
 ?>
 
-<?php require_once 'header.php'; ?>
+<!DOCTYPE html>
+<html>
 
-<div class="container">
-    <?php if (!$error) : ?>
-        <h1>Detalhes do Produto:</h1>
+<head>
+    <title>Editar Produto</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
 
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $product_name; ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted">Postado em <?php echo $product_date; ?></h6>
-                <p class="card-text"><?php echo $product_description; ?></p>
-                <p class="card-text">Preço: <?php echo $product_price; ?></p>
-                <p class="card-text">Anunciante: <?php echo $vendor_name; ?></p>
-                <a class="btn btn-success my-2" href="<?php echo isUserLoggedIn() ? htmlspecialchars($_SERVER['PHP_SELF']) . "?id=$product_id&mode=register" : "login.php"; ?>">Contatar</a>
-                <a class="btn btn-success my-2" <?php echo isUserLoggedIn() ? "href='https://api.whatsapp.com/send?phone=+55$User_Number&text=Olá, tudo bem?' target='_blank'" : "href=login.php"; ?>><i class="fa-brands fa-whatsapp fa-spin"></i></a>
+<body>
+    <div class="container mt-5">
+        <h1>Editar Produto</h1>
+        <form action="" method="POST">
+            <input type="hidden" name="id" value="<?php echo $product['Product_Id']; ?>">
+            <div class="form-group">
+                <label for="name">Nome</label>
+                <input type="text" class="form-control" id="name" name="name" value="<?php echo $product['Product_Name']; ?>" required>
             </div>
-        </div>
+            <div class="form-group">
+                <label for="price">Preço</label>
+                <input type="number" class="form-control" id="price" name="price" value="<?php echo $product['Product_Price']; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Descrição</label>
+                <textarea class="form-control" id="description" name="description" rows="3" required><?php echo $product['Product_Description']; ?></textarea>
+            </div>
 
-        <h2>Imagens</h2>
-        <div class="row">
-            <?php foreach ($images as $image) : ?>
-                <div class="col-md-3 mb-3">
-                    <img src="<?php echo $image; ?>" class="img-fluid" alt="Product Image">
-                </div>
+            <h2>Imagens do Produto</h2>
+            <?php foreach ($images as $image): ?>
+                <img src="<?php echo $image['Image_Path']; ?>" alt="Product Image" width="200px">
+                <a href="excluirimagem.php?id=<?php echo $image['Image_Id']; ?>&product_id=<?php echo $product['Product_Id']; ?>">Excluir</a>
+                <hr>
             <?php endforeach; ?>
-        </div>
-        
-        <!-- Formulário para adicionar uma nova imagem -->
-        <h2>Adicionar Imagem</h2>
-        <form action="adicionarimagem.php" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
-            <div class="form-group">
-                <label for="image">Selecione a imagem</label>
-                <input type="file" class="form-control-file" id="image" name="image" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Adicionar Imagem</button>
-        </form>
-        
-        <!-- Formulário para excluir uma imagem -->
-        <h2>Excluir Imagem</h2>
-        <form action="excluirimagem.php" method="POST">
-            <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
-            <div class="form-group">
-                <label for="image_select">Selecione a imagem para excluir</label>
-                <select class="form-control" id="image_select" name="image_select">
-                    <?php foreach ($images as $image) : ?>
-                        <option value="<?php echo $image; ?>"><?php echo $image; ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-danger">Excluir Imagem</button>
-        </form>
-        
-        <!-- Formulário para alterar uma imagem -->
-        <h2>Alterar Imagem</h2>
-        <form action="alterarimagem.php" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
-            <div class="form-group">
-                <label for="old_image_select">Selecione a imagem para alterar</label>
-                <select class="form-control" id="old_image_select" name="old_image_select">
-                    <?php foreach ($images as $image) : ?>
-                        <option value="<?php echo $image; ?>"><?php echo $image; ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="new_image">Selecione a nova imagem</label>
-                <input type="file" class="form-control-file" id="new_image" name="new_image" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Alterar Imagem</button>
-        </form>
-        
-        <a href="index.php" class="btn btn-danger"><i class="fas fa-undo"></i> Voltar</a>
 
-    <?php else :
-        echo "<p>$error</p>";
-    endif;
-    ?>
-</div>
+            <h3>Adicionar Imagem</h3>
+            <form action="adicionarimagem.php" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="product_id" value="<?php echo $product['Product_Id']; ?>">
+                <div class="form-group">
+                    <input type="file" name="image" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Adicionar Imagem</button>
+            </form>
 
-<?php require_once 'footer.php'; ?>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+            <a href="meusprodutos.php" class="btn btn-secondary">Cancelar</a>
+        </form>
+    </div>
+</body>
+
+</html>
