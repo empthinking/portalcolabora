2815a822338e8069153eee38cec01a6226abced0
empthinking <117464630+empthinking@users.noreply.github.com>
2023-06-13 14:44:52 -0300

    Atualizar o register.php

--- register.php
+++ register.php
@@ -109,23 +109,28 @@ require_once 'footer.php';
 
 $url = htmlspecialchars(trim($_SERVER['PHP_SELF']));
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>User Registration Form</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!DOCTYPE html> 
+ <html lang="pt-br"> 
+ <head> 
+   <meta charset="UTF-8"> 
+   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+   <title>Cadastro</title> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="./img/favicon-32x32.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
-    <style>
-    .container {
-        max-width: 600px;
-    }
-    </style>
-</head>
+<style>
+     body { 
+           min-height: 100vh; 
+           display: flex; 
+           flex-direction: column; 
+     } 
+  
+   </style> 
+ <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
+ <script src="https://kit.fontawesome.com/0280b9824e.js" crossorigin="anonymous"></script> 
+ </head>
 
 <body>
     <div class="container mt-4">
