403cdbf76e84c6aec270efff9b1907c7464fe497
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-13 22:43:16 -0300

    Create excluir_imagem.php

---
+++ admin/excluir_imagem.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'db.php';
+
+session_start();
+
+if (!isUserLoggedIn()) {
+    header('Location: index.php');
+    exit();
+}
+
+$image_id = $_GET['id'];
+$product_id = $_GET['product_id'];
+
+// Verificar se a imagem pertence ao produto do usuário logado
+$stmt = $db->prepare('SELECT * FROM Images WHERE Image_Id = ? AND Product_Id = ?');
+$stmt->bind_param('ii', $image_id, $product_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 0) {
+    // A imagem não pertence ao produto ou não existe
+    header("Location: editarproduto.php?id=$product_id");
+    exit();
+}
+
+// Excluir a imagem do banco de dados e do diretório
+$image = $result->fetch_assoc();
+$image_path = $image['Image_Name'];
+
+$deleteStmt = $db->prepare("DELETE FROM Images WHERE Image_Id = ?");
+$deleteStmt->bind_param("i", $image_id);
+
+if ($deleteStmt->execute()) {
+    if (unlink($image_path)) {
+        $_SESSION['message'] = "Imagem excluída com sucesso!";
+    } else {
+        $_SESSION['message'] = "Falha ao excluir a imagem do diretório.";
+    }
+} else {
+    $_SESSION['message'] = "Falha ao excluir a imagem do banco de dados.";
+}
+
+$stmt->close();
+
+header("Location: editarproduto.php?id=$product_id");
+exit();
+?>
