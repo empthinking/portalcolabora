31facd3af8869cc2b4ae037ddba0292e63b4e00a
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 20:43:01 -0300

    Update index.php

--- admin/teste/index.php
+++ admin/teste/index.php
@@ -1,16 +1,38 @@
 <?php
 require_once "dbconn.php";
+
 // Verificar se o formulário foi submetido para validar o código
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['verify_code'])) {
-$chave = $_SESSION['chave'];
-
     $verificationCode = $_POST['verification_code'];
 
     // Validar o código de verificação
     if (validateVerificationCode($verificationCode)) {
-        // Redirecionar para a página de teste com a chave na URL
-        header("Location: teste.php?id=" . urlencode($chave));
-        exit();
+        // Obter a chave codificada da URL
+        $encodedKey = $_GET['chave'];
+
+        // Decodificar a chave
+        $decodedKey = base64_decode($encodedKey);
+
+        // Descriptografar a chave
+        $decryptedKey = decryptKey($decodedKey);
+
+        // Obter os dados (ID e email) da chave descriptografada
+        list($userId, $email) = explode('|', $decryptedKey);
+
+        // Validar os dados com a tabela User
+        if (validateUser($userId, $email)) {
+            // Iniciar a sessão para o usuário
+            session_start();
+            $_SESSION['user_id'] = $userId;
+            $_SESSION['email'] = $email;
+
+            // Redirecionar para a página de teste
+            header("Location: teste.php");
+            exit();
+        } else {
+            // Dados inválidos, exibir mensagem de erro
+            echo '<div class="alert alert-danger">Dados inválidos. Acesso negado.</div>';
+        }
     } else {
         // Código inválido, exibir mensagem de erro
         echo '<div class="alert alert-danger">Código de verificação inválido. Acesso negado.</div>';
@@ -19,7 +41,6 @@ $chave = $_SESSION['chave'];
 
 // Função para validar o código de verificação
 function validateVerificationCode($code) {
-
     global $connection;
 
     $stmt = $connection->prepare("SELECT COUNT(*) FROM verification_codes WHERE code = ?");
@@ -32,7 +53,30 @@ function validateVerificationCode($code) {
     return $count > 0;
 }
 
+// Função para descriptografar a chave
+function decryptKey($encryptedKey) {
+    // Aqui você deve implementar o algoritmo de descriptografia adequado
+    // Utilize a lógica de descriptografia que você possui
 
+    // Exemplo: descriptografia simples com base64_decode
+    $decryptedKey = base64_decode($encryptedKey);
+
+    return $decryptedKey;
+}
+
+// Função para validar os dados do usuário
+function validateUser($userId, $email) {
+    global $connection;
+
+    $stmt = $connection->prepare("SELECT COUNT(*) FROM User WHERE User_Id = ? AND User_Email = ?");
+    $stmt->bind_param("is", $userId, $email);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+
+    return $count > 0;
+}
 ?>
 
 <!DOCTYPE html>
@@ -46,6 +90,7 @@ function validateVerificationCode($code) {
     <div class="container">
         <h1>Verificação de Código</h1>
 
+        <!-- Formulário para verificar o código de verificação -->
         <form method="POST" action="index.php">
             <div class="mb-3">
                 <label for="verification_code" class="form-label">Código de Verificação:</label>
@@ -54,12 +99,10 @@ function validateVerificationCode($code) {
             <button type="submit" class="btn btn-primary" name="verify_code">Verificar</button>
         </form>
 
-        <form method="POST" action="send_verification_email.php">
-            <button type="submit" class="btn btn-secondary" name="send_email">Enviar Código por E-mail</button>
+        <!-- Formulário para gerar e validar a chave -->
+        <form method="POST" action="validate_key.php">
+            <button type="submit" class="btn btn-secondary" name="validate_key">Validar Chave</button>
         </form>
     </div>
-
-    <!-- Adicione os scripts do Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
