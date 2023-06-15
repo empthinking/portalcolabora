9ad916d72d9e304fbe04cc131d0a5deb8d81a7f9
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 11:43:32 -0300

    Update index.php

--- admin/teste/index.php
+++ admin/teste/index.php
@@ -1,3 +1,76 @@
+<?php
+require_once "dbconn.php";
+
+// Verificar se o formulário foi submetido para validar o código
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['verify_code'])) {
+    // Obter o código digitado pelo usuário
+    $verificationCode = $_POST['verification_code'];
+
+    // Validar o código de verificação
+    if (validateVerificationCode($verificationCode)) {
+        // Código válido
+        echo '<div class="alert alert-success">Código de verificação válido. Acesso permitido.</div>';
+        header("teste.php");
+        exit();
+    } else {
+        // Código inválido ou expirado
+        echo '<div class="alert alert-danger">Código de verificação inválido ou expirado. Acesso negado.</div>';
+    }
+}
+
+// Função para remover o código de verificação do banco de dados
+function removeVerificationCode($code) {
+    global $connection;
+
+    // Preparar a consulta SQL para remover o código
+    $stmt = $connection->prepare("DELETE FROM verification_codes WHERE code = ?");
+    $stmt->bind_param("s", $code);
+
+    // Executar a consulta
+    if ($stmt->execute()) {
+        // Código de verificação removido com sucesso
+    } else {
+        // Tratar o erro de remoção
+    }
+
+    // Fechar a declaração
+    $stmt->close();
+}
+
+// Função para validar o código de verificação
+function validateVerificationCode($code) {
+    global $connection;
+
+    // Verificar se o código existe no banco de dados
+    $stmt = $connection->prepare("SELECT expiry_time FROM verification_codes WHERE code = ? LIMIT 1");
+    $stmt->bind_param("s", $code);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows === 1) {
+        $expiryTime = null; // Inicializar a variável $expiryTime
+        $stmt->bind_result($expiryTime);
+        $stmt->fetch();
+
+        // Verificar se o código ainda está válido (não expirado)
+        if (time() <= $expiryTime) {
+            // Código válido
+            $stmt->close();
+
+            // Remover o código de verificação do banco de dados após 1 minuto
+            sleep(60); // Aguardar 1 minuto
+            removeVerificationCode($code);
+
+            return true;
+        }
+    }
+
+    // Código inválido ou expirado
+    $stmt->close();
+    return false;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,21 +81,6 @@
 <body>
     <div class="container">
         <h1>Verificação de Código</h1>
-
-        <?php
-        // Verificar se o formulário foi submetido para validar o código
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['verify_code'])) {
-            // Obter o código digitado pelo usuário
-            $verificationCode = $_POST['verification_code'];
-
-            // Validar o código de verificação
-            if (validateVerificationCode($verificationCode)) {
-                echo '<div class="alert alert-success">Código de verificação válido. Acesso permitido.</div>';
-            } else {
-                echo '<div class="alert alert-danger">Código de verificação inválido ou expirado. Acesso negado.</div>';
-            }
-        }
-        ?>
 
         <form method="POST" action="index.php">
             <div class="mb-3">
