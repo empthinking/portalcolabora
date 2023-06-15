2e8eaffb7574e3f6e0a74c251b9b1cf07c5e5b31
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 14:17:58 -0300

    Update teste.php

--- admin/teste/teste.php
+++ admin/teste/teste.php
@@ -3,42 +3,48 @@ require_once "dbconn.php";
 
 // Verificar se a chave está presente na URL
 if (isset($_GET['chave'])) {
+    // Obter a chave da URL
     $chave = $_GET['chave'];
 
     // Verificar se a chave é válida
     if (validateChave($chave)) {
-        // Buscar os dados do usuário
+        // Obter as informações do usuário com base no User_Id relacionado à chave
         $user = getUserByChave($chave);
 
+        // Verificar se o usuário foi encontrado
         if ($user) {
-            // Exibir os dados do usuário
+            // Exibir as informações do usuário
             echo "<h1>Dados do Usuário</h1>";
-            echo "<p><strong>Nome:</strong> " . $user['User_Name'] . "</p>";
-            echo "<p><strong>Email:</strong> " . $user['User_Email'] . "</p>";
+            echo "<p>Nome: " . $user['User_Nome'] . "</p>";
+            echo "<p>E-mail: " . $user['User_Email'] . "</p>";
         } else {
             // Usuário não encontrado
-            echo "<div class='alert alert-danger'>Usuário não encontrado.</div>";
+            echo '<div class="alert alert-danger">Usuário não encontrado.</div>';
         }
     } else {
         // Chave inválida
-        echo "<div class='alert alert-danger'>Chave inválida. Acesso negado.</div>";
+        echo '<div class="alert alert-danger">Chave inválida. Acesso negado.</div>';
     }
 } else {
-    // Chave não fornecida
-    echo "<div class='alert alert-danger'>Chave não fornecida. Acesso negado.</div>";
+    // Chave não presente na URL
+    echo '<div class="alert alert-danger">Acesso negado. Chave não fornecida.</div>';
 }
 
 // Função para validar a chave
 function validateChave($chave) {
     // Implemente a lógica de validação da chave aqui
-    // Por exemplo, você pode consultar o banco de dados para verificar se a chave é válida
+    // Por exemplo, verifique se a chave existe no banco de dados ou se atende a algum critério específico
     // Retorne true se a chave for válida, ou false caso contrário
 
-    // Exemplo de lógica de validação da chave:
+    // Exemplo de lógica de validação:
     global $connection;
 
-    $stmt = $connection->prepare("SELECT COUNT(*) FROM verification_codes WHERE chave = ?");
-    $stmt->bind_param("s", $chave);
+    $stmt = $connection->prepare("SELECT COUNT(*) FROM Users WHERE User_Id = ? AND Chave = ?");
+    $stmt->bind_param("ss", $userId, $chave);
+
+    // Obter o User_Id relacionado à chave
+    $userId = getUserIdByChave($chave);
+
     $stmt->execute();
     $stmt->bind_result($count);
     $stmt->fetch();
@@ -47,46 +53,48 @@ function validateChave($chave) {
     return $count > 0;
 }
 
-// Função para buscar os dados do usuário com base na chave
-function getUserByChave($chave) {
-    // Implemente a lógica para buscar os dados do usuário com base na chave
-    // Por exemplo, você pode consultar o banco de dados para obter os dados do usuário
-    // Retorne os dados do usuário como um array associativo, ou null se o usuário não for encontrado
+// Função para obter o User_Id relacionado à chave
+function getUserIdByChave($chave) {
+    // Implemente a lógica para obter o User_Id relacionado à chave
+    // Por exemplo, consulte o banco de dados para obter o User_Id
+    // Retorne o User_Id
 
-    // Exemplo de busca do usuário pelo User_Id (considerando que a chave é o User_Id em formato sha1):
+    // Exemplo de lógica:
     global $connection;
 
-    $stmt = $connection->prepare("SELECT User_Name, User_Email FROM User WHERE SHA1(User_Id) = ?");
+    $stmt = $connection->prepare("SELECT User_Id FROM Users WHERE Chave = ?");
     $stmt->bind_param("s", $chave);
     $stmt->execute();
-    $stmt->bind_result($name, $email);
+    $stmt->bind_result($userId);
     $stmt->fetch();
     $stmt->close();
 
-    if ($name && $email) {
+    return $userId;
+}
+
+// Função para obter as informações do usuário com base no User_Id
+function getUserByChave($chave) {
+    // Implemente a lógica para obter as informações do usuário com base no User_Id
+    // Por exemplo, consulte o banco de dados para obter o nome e e-mail do usuário
+    // Retorne as informações do usuário em um array associativo
+
+    // Exemplo de lógica:
+    global $connection;
+
+    $stmt = $connection->prepare("SELECT User_Nome, User_Email FROM Users WHERE Chave = ?");
+    $stmt->bind_param("s", $chave);
+    $stmt->execute();
+    $stmt->bind_result($nome, $email);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($nome && $email) {
         return array(
-            'User_Name' => $name,
+            'User_Nome' => $nome,
             'User_Email' => $email
         );
+    } else {
+        return false;
     }
-
-    return null;
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Página de Teste</title>
-    <!-- Adicione os links para os arquivos CSS do Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <!-- O conteúdo da página de teste -->
-    </div>
-
-    <!-- Adicione os scripts do Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
