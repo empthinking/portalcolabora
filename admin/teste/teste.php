4ff532dbb9b73c3e79934e05520bd249f97ea95f
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 13:11:30 -0300

    Update teste.php

--- admin/teste/teste.php
+++ admin/teste/teste.php
@@ -14,14 +14,40 @@ $sessionToken = $_SESSION['session_token'];
 // Verificar se a página foi acessada após fechar a janela do navegador
 if (!isset($_COOKIE['session_token']) || $_COOKIE['session_token'] !== $sessionToken) {
     // Token de sessão inválido ou ausente, redirecionar de volta para a página de verificação
-echo "erro";
+    header("Location: index.php");
     exit();
 }
 
 // Definir um novo cookie para atualizar a expiração do token de sessão
 setcookie('session_token', $sessionToken, time() + 60);
 
-// Resto do conteúdo da página "teste.php" aqui
+// Verificar se a chave está presente na URL
+if (isset($_GET['chave'])) {
+    // Obter a chave da URL
+    $chave = $_GET['chave'];
+
+    // Pesquisar no banco de dados usando a chave
+    $stmt = $connection->prepare("SELECT User_Id, User_Email FROM Users WHERE SHA1(CONCAT(User_Email, User_Id)) = ?");
+    $stmt->bind_param("s", $chave);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Verificar se a chave é válida
+    if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+
+        // Extrair o ID e o email do resultado da consulta
+        $id = $row['User_Id'];
+        $email = $row['User_Email'];
+
+        // Exibir o ID e o email
+        echo "ID: " . $id . "<br>";
+        echo "Email: " . $email . "<br>";
+    } else {
+        // Chave inválida
+        echo "Chave inválida";
+    }
+}
 ?>
 
 <!DOCTYPE html>
