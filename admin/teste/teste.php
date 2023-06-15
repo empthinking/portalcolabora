597db8392c8a2392716ad02fd75e636f631d1d9b
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 20:44:31 -0300

    Update teste.php

--- admin/teste/teste.php
+++ admin/teste/teste.php
@@ -1,16 +1,1 @@
-<?php
-// Verificar se a chave está presente na URL
-if (isset($_GET['chave'])) {
-    // Obter o valor da chave
-    $chaveCodificada = $_GET['chave'];
-
-    // Decodificar a chave
-    $chaveDecodificada = base64_decode($chaveCodificada);
-
-    // Exibir o valor da chave decodificada
-    echo "Chave: " . $chaveDecodificada;
-} else {
-    // A chave não está presente na URL
-    echo "Chave não encontrada.";
-}
-?>
+ok
