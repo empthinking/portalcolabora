1d1e9cf8f5b4544a5c87d0ab7814c6fda8e810da
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 14:39:04 -0300

    Update teste.php

--- admin/teste/teste.php
+++ admin/teste/teste.php
@@ -1,22 +1,16 @@
 <?php
-// Inicie a sessão
-session_start();
-
-// Verifique se a chave está presente na URL
+// Verificar se a chave está presente na URL
 if (isset($_GET['chave'])) {
-    $chave = $_GET['chave'];
+    // Obter o valor da chave
+    $chaveCodificada = $_GET['chave'];
 
-    // Verifique se a chave corresponde à chave armazenada na sessão
-    if ($_SESSION['chave'] === $chave) {
-        // Chave válida, permita o acesso ao conteúdo da página
-        echo "Bem-vindo à página de teste!";
-    } else {
-        // Chave inválida, exiba uma mensagem de erro
-        echo "Acesso negado. Chave inválida.";
-    }
+    // Decodificar a chave
+    $chaveDecodificada = base64_decode($chaveCodificada);
+
+    // Exibir o valor da chave decodificada
+    echo "Chave: " . $chaveDecodificada;
 } else {
-    // Chave não fornecida, redirecione para a página de verificação de código
-    header("Location: index.php");
-    exit();
+    // A chave não está presente na URL
+    echo "Chave não encontrada.";
 }
 ?>
