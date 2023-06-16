0aa835bc9c8739c57d6512eba66cbc3ef8b6f509
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2023-06-15 21:45:47 -0300

    Update index.php

--- admin/teste/index.php
+++ admin/teste/index.php
@@ -1,56 +1,61 @@
 <?php
 require_once "dbconn.php";
 
-// Verificar se o código de verificação está presente na URL
-if (isset($_GET['code'])) {
-    $verificationCode = $_GET['code'];
-
-    // Validar o código de verificação
-    if (validateVerificationCode($verificationCode)) {
-        // Redirecionar para a página de teste com a chave na URL
-        header("Location: teste.php?code=" . urlencode($verificationCode));
-        exit();
-    } else {
-        echo 'Código de verificação inválido.';
-    }
-}
-
-// Função para validar o código de verificação
-function validateVerificationCode($code) {
+function checkVerificationCode($userEmail, $userId, $code) {
     global $connection;
 
-    $stmt = $connection->prepare("SELECT COUNT(*) FROM verification_codes WHERE code = ? AND expiry_time >= ?");
-    $stmt->bind_param("si", $code, time());
-    $stmt->execute();
-    $stmt->bind_result($count);
-    $stmt->fetch();
-    $stmt->close();
+    $currentTime = time();
 
-    return $count > 0;
+    // Consultar o banco de dados para verificar se o código existe e não expirou
+    $stmt = $connection->prepare("SELECT expiry_time FROM verification_codes WHERE User_Email = ? AND User_Id = ? AND code = ?");
+    $stmt->bind_param("sis", $userEmail, $userId, $code);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($expiryTime);
+        $stmt->fetch();
+
+        if ($expiryTime >= $currentTime) {
+            // O código existe e não expirou
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function markEmailAsVerified($userEmail, $userId) {
+    global $connection;
+
+    // Atualizar o status de verificação do e-mail no banco de dados
+    $stmt = $connection->prepare("UPDATE Users SET EmailVerified = 1 WHERE User_Email = ? AND User_Id = ?");
+    $stmt->bind_param("si", $userEmail, $userId);
+    $stmt->execute();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
+    $chave = $_GET["id"];
+    $decodedKey = base64_decode($chave);
+    $keyParts = explode("|", $decodedKey);
+
+    $userEmail = $keyParts[0];
+    $userId = $keyParts[1];
+    $code = $_GET["code"];
+
+    $verificationCodeValid = checkVerificationCode($userEmail, $userId, $code);
+
+    if ($verificationCodeValid) {
+        // Código de verificação válido
+        markEmailAsVerified($userEmail, $userId);
+
+        // Exibir o conteúdo da chave descriptografada
+        echo "<h1>Chave decriptografada</h1>";
+        echo "<p>Email: $userEmail</p>";
+        echo "<p>ID do usuário: $userId</p>";
+    } else {
+        // Código de verificação inválido ou expirado
+        echo "<h1>Código de verificação inválido ou expirado</h1>";
+    }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Verificação de Código</title>
-    <!-- Adicione o link para o arquivo CSS do Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Verificação de Código</h1>
-
-        <form method="POST" action="index.php">
-            <div class="mb-3">
-                <label for="verification_code" class="form-label">Código de Verificação:</label>
-                <input type="text" class="form-control" id="verification_code" name="verification_code" required>
-            </div>
-            <button type="submit" class="btn btn-primary" name="verify_code">Verificar</button>
-        </form>
-    </div>
-
-    <!-- Adicione o script do Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
