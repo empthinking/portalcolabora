59091e5576bd2833d01bd259b0f7b7acc7af9639
empthinking <117464630+empthinking@users.noreply.github.com>
2023-06-18 22:12:15 -0300

    Update message_details.php

--- message_details.php
+++ message_details.php
@@ -1,7 +1,12 @@
 <?php
 $sql_prep= 'SELECT m.Message_Sender, m.Message_Content, u.User_Name FROM Messages m JOIN Users u ON m.Message_Receiver = u.User_Id WHERE m.Message_Receiver = ? AND m.Message_Id = ?';
 $stmt = $db->prepare($sql_prep);
-$stmt = $db->bind_param('ii', $_SESSION['id'], $messageId);
+$stmt->bind_param('ii', $_SESSION['id'], $messageId);
+$stmt->execute();
+$result = $stmt->get_result();
+$message = $result->fetch_assoc();
+$result->close();
+$stmt->close();
 
 
 require_once 'header.php';
