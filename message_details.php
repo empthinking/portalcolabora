c7f2a91479d76ac8b538e0386b079baca9f0ed0f
empthinking <117464630+empthinking@users.noreply.github.com>
2023-06-18 22:29:29 -0300

    Update message_details.php

--- message_details.php
+++ message_details.php
@@ -1,5 +1,5 @@
 <?php
-$sql_prep= 'SELECT m.Message_Sender, m.Message_Content, u.User_Name FROM Messages m JOIN Users u ON m.Message_Receiver = u.User_Id WHERE m.Message_Receiver = ? AND m.Message_Id = ?';
+$sql_prep= 'SELECT m.Message_Id, m.Message_Sender, m.Message_Readed, m.Message_Content, u.User_Name FROM Messages m JOIN Users u ON m.Message_Receiver = u.User_Id WHERE m.Message_Receiver = ? AND m.Message_Id = ?';
 $stmt = $db->prepare($sql_prep);
 $stmt->bind_param('ii', $_SESSION['id'], $messageId);
 $stmt->execute();
@@ -7,7 +7,7 @@ $result = $stmt->get_result();
 $message = $result->fetch_assoc();
 $result->close();
 $stmt->close();
-
+if($message['Message_Readed'] === 0) $markMessagesAsRead($message['Message_Id']); 
 
 require_once 'header.php';
 
